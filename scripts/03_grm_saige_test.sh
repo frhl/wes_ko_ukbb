c62b458d4b6a0a89dcf406c33eecb240f3f8067d
frhl <frederik.heymann@gmail.com>
2021-09-09 09:47:28 +0100

    generate input for grm

--- scripts/03_grm_saige_test.sh
+++ scripts/03_grm_saige_test.sh
@@ -1,52 +1,58 @@
 #!/usr/bin/env bash
 #
 #
-#$ -N create_grm
+#$ -N test_grm
 #$ -wd /well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb
-#$ -o logs/create_grm.log
-#$ -e logs/create_grm.errors.log
+#$ -o logs/test_grm.log
+#$ -e logs/test_grm.errors.log
 #$ -P lindgren.prjc
-#$ -pe shmem 20
+#$ -pe shmem 2
 #$ -q short.qe
 
 set -o errexit
 set -o nounset
 
 module purge
-source utils/qsub_utils.sh
-source utils/hail_utils.sh
+#source utils/qsub_utils.sh
+#source utils/hail_utils.sh
+source utils/test_utils.sh
 
 # paths for hail script
-readonly out_dir="data/saige/grm/input"
-readonly out_prefix="${out_dir}/ukb_imp_chr1_22_sparse_markers"
+readonly out_dir="data/saige/grm/input/test"
+readonly out_prefix="${out_dir}/ukb_imp_sparse_markers"
 readonly hail_script="utils/create_grm_input.py"
 readonly spark_dir="data/tmp/spark"
 readonly threads=$(( ${NSLOTS}-1 ))
 readonly createSparseGRM="/well/lindgren/flassen/software/dev/SAIGE/extdata/createSparseGRM.R"
 
 # Generate a sequence of chromosomes to be included
-chroms=$( seq 1 22 | tr '\n' ' ' )
+chroms=$( seq 21 22 | tr '\n' ' ' )
 
 # combine markers from UKBB imputed data
-set_up_hail
-set_up_pythonpath
-mkdir -p ${out_dir}
-python "${hail_script}" \
-	--chroms ${chroms}  \
-	--out_prefix ${out_prefix} \
-	--subset_markers_by_kinship
+#set_up_hail
+#set_up_pythonpath
+#mkdir -p ${out_dir}
+#python "${hail_script}" \
+#	--chroms ${chroms}  \
+#	--out_prefix ${out_prefix} \
+#	--subset_markers_by_kinship
 
-print_update "Successfully combined .bgen files for GRM input." "${SECONDS}"
+#print_update "Successfully combined .bgen files for GRM input." "${SECONDS}"
 
 
 # compute GRM with SAIGE
-if [ $( ls -1 *.mtx 2> /dev/null | wc -l ) == 0 ]; then
-	conda deactivate
+if [ $( ls -1 ${out_prefix}*.mtx 2> /dev/null | wc -l ) == 0 ]; then
+	#module load Anaconda3/2020.07
+  	#module load java/1.8.0_latest
+  	#set_up_conda
+	#source "/apps/eb/skylake/software/Anaconda3/2020.07/etc/profile.d/conda.sh"
+  	#conda activate RSAIGE
+
 	set_up_RSAIGE
 	print_update "Generating GRM from plink files.. "
 	Rscript "${createSparseGRM}" \
 		--plinkFile=${out_prefix} \
-		--nThreads=4 \
+		--nThreads=1 \
 		--outputPrefix=${out_prefix}	\
 		--numRandomMarkerforSparseKin=1000	\
 		--relatednessCutoff=0.125
---
+++ utils/test_utils.sh
@@ -0,0 +1,113 @@
+#!/usr/bin/env bash
+
+# set paths to executables
+export PATH="${HOME}/htslib_1.11/bin:${PATH}"   # path to htslib 1.11
+
+
+# utility functions
+raise_error() {
+  >&2 echo -e "Error: $1. Exiting." && exit 1
+}
+
+get_chr() {
+  if [ $1 -eq 23 ]; then
+    echo "X"
+  elif [ $1 -eq 24 ]; then
+    echo "Y"
+  elif [ $1 -ge 1 ] && [ $1 -le 22 ]; then
+    echo $1
+  else 
+    raise_error "chromosome number must be between 1 and 24"
+  fi
+}
+
+elapsed_time() {
+  if [ ! -z $1 ]; then echo "elapsed time: $( echo "scale=2; $1/3600" | bc -l ) hrs "; fi
+}
+
+print_update() {
+  local _message=$1
+  set +o nounset # disable check for unbound variables temporarily
+  local _duration=$2
+  echo "${_message} $( elapsed_time ${_duration} )(job id: ${JOB_ID}.${SGE_TASK_ID} $( date ))"
+  set -o nounset
+}
+
+vcf_check() {
+  if [ ! -f $1 ]; then # check that VCF exists
+    raise_error "$1 does not exist"
+  elif [ ! -s $1 ]; then # check that VCF is not an empty file
+    raise_error "$1 exists but is empty"
+  elif [ $( bcftools view -h $1 2>&1 | head | grep "No BGZF EOF marker" | wc -l ) -gt 0 ]; then # check that VCF is not truncated
+    raise_error "$1 may be truncated"
+  fi
+}
+
+make_tabix() {
+  if [ ! -f $1.tbi ]; then
+    local _start_time=${SECONDS}
+    bcftools index $1 \
+      --tbi \
+      --threads $(( ${NSLOTS}-1 ))
+    local _duration=$(( ${SECONDS}-${_start_time} ))
+    print_update "finished tabix of $1" "${_duration}"
+  fi
+}
+
+set_up_conda() {
+  local __conda_setup="$('/apps/eb/skylake/software/Anaconda3/2020.07/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+  if [ $? -eq 0 ]; then
+      set +o nounset # use to avoid "PS1: unbound variable" error
+      eval "$__conda_setup"
+      set -o nounset
+  else
+      if [ -f "/apps/eb/skylake/software/Anaconda3/2020.07/etc/profile.d/conda.sh" ]; then
+          . "/apps/eb/skylake/software/Anaconda3/2020.07/etc/profile.d/conda.sh"
+      else
+          export PATH="/apps/eb/skylake/software/Anaconda3/2020.07/bin:$PATH"
+      fi
+  fi
+  unset __conda_setup
+}
+
+get_hail_memory() {
+  if [[ -z ${QUEUE} || -z ${NSLOTS} ]]; then
+    raise_error "QUEUE and NSLOTS must both be defined"
+  fi
+  if [[ "${QUEUE}" = *".qe" || "${QUEUE}" = *".qc" ]]; then
+    local _mem_per_slot=10
+  elif [[ "${QUEUE}" = *".qf" ]]; then
+    local _mem_per_slot=3
+  else
+    raise_error "QUEUE must end in either \".qe\", \".qc\", or \".qf\""
+  fi
+  echo $(( ${_mem_per_slot}*${NSLOTS} ))
+}
+
+set_up_hail() {
+  mkdir -p ${spark_dir} # directory for Hail's temporary Spark output files
+  module load Anaconda3/2020.07
+  module load java/1.8.0_latest
+  set_up_conda
+  conda activate jupyter-hail # Requires conda environment with Hail installed
+  local _mem=$( get_hail_memory ) 
+  if [ ! -z ${_mem} ]; then
+    export PYSPARK_SUBMIT_ARGS="--conf spark.local.dir=${spark_dir} --conf spark.executor.heartbeatInterval=1000000 --conf spark.network.timeout=1000000  --driver-memory ${_mem}g --executor-memory ${_mem}g pyspark-shell"
+  fi
+}
+
+set_up_RSAIGE() {
+  module load Anaconda3/2020.07
+  module load java/1.8.0_latest
+  source "/apps/eb/skylake/software/Anaconda3/2020.07/etc/profile.d/conda.sh"
+  conda activate RSAIGE
+}
+
+
+set_up_vep() {
+  module load EnsEMBLCoreAPI/96.0-r20190601-foss-2019a-Perl-5.28.1 # required for LOFTEE
+  module load VEP/95.0-foss-2018b-Perl-5.28.0 # required FOR VEP (NOTE: this steps throws some errors since the above module is already loaded. It works nonetheless.)
+  module load samtools/1.8-gcc5.4.0 # required for LOFTEE 
+  export PERL5LIB=$PERL5LIB:/well/lindgren/flassen/software/VEP/plugins_grch38/
+}
+
