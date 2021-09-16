9fbfee5b2b8efd59e4c98a0029533afea7877524
frhl <frederik.heymann@gmail.com>
2021-09-16 10:31:54 +0100

    updated readme and notebooks

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # wes_ko_ukbb
 
+Counts can be found here https://docs.google.com/spreadsheets/d/1p3wvZx7BNcMDg2BBT1wNSpVjC9gEwOloDEqeKMrcRAM/edit#gid=0
+
+
 updated 15-sep-2021
 
 ## todo
-* use UKBB kinship markers to generate a sparse matrix of all chromosomes
-* use these markers to generate a sparse matrix (also filter by 95% missingness)
-* Should result in around 93k markers in total.
+* (DONE) use UKBB kinship markers to generate a sparse matrix of all chromosomes
+* (DONE) use these markers to generate a sparse matrix (also filter by 95% missingness)
+* (DONE) Should result in around 93k markers in total.
 * run saige with with sparse matrix and fake VCF files that represent knockouts. (Notice, that we treat these files as single markers and not genesets!).
-
 
 ## Current pipeline:
 * Core functions are found in utils/hail_export.py
--- utils/hail_vep_export.sh
+++ utils/hail_vep_export.sh
@@ -14,6 +14,7 @@ def main(args):
     input_type = args.input_type
     out_prefix = args.out_prefix
     out_type = args.out_type
+    translate_iid = args.translate_iid
     
     # annotate with VEP
     hail_init.hail_bmrc_init('/logs/hail/hail_vep_export.log', 'GRCh38')
@@ -25,7 +26,8 @@ def main(args):
     dataset = qc.default_to_snpid_when_missing_rsid(dataset)
 
     # Translate to lindgren IDs
-    dataset = qc.translate_sample_ids(dataset, 12788, 11867)    
+    if translate_iid:
+        dataset = qc.translate_sample_ids(dataset, 12788, 11867)    
     
     # recalc info
     dataset = qc.recalc_info(dataset)
@@ -40,9 +42,14 @@ if __name__=='__main__':
     # initial params
     parser.add_argument('--input_path', default=None, help='Path to input')
     parser.add_argument('--input_type', default=None, help='Input type, either "mt", "vcf" or "plink"')
+    parser.add_argument('--translate_iid', action='store_true', help='Translate iid to lindgren data (Only required for phased data)')
     parser.add_argument('--out_prefix', default=None, help='Path prefix for output dataset')
     parser.add_argument('--out_type', default=None, help='Type of output dataset (options: mt, vcf, plink)')
     args = parser.parse_args()
 
     main(args)
+
+
+
+
 
