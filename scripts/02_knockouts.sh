623c1df5c47e9e34224a24a431f93c30a86b275c
frhl <frederik.heymann@gmail.com>
2021-09-22 09:27:31 +0100

    HAIL pipeline (VEP+rsid+processing) successfully split up

--- scripts/02_knockouts.sh
+++ scripts/02_knockouts.sh
@@ -7,7 +7,7 @@
 #$ -P lindgren.prjc
 #$ -pe shmem 10
 #$ -q short.qe
-#$ -t 19
+#$ -t 22
 
 set -o errexit
 set -o nounset
@@ -16,8 +16,7 @@ source utils/qsub_utils.sh
 source utils/hail_utils.sh
 
 # directories
-readonly in_dir_phased="data/phased"
-readonly in_dir_unphased="data/unphased/unfiltered"
+readonly in_dir="data/mt"
 readonly vep_dir="data/vep/full/"
 readonly spark_dir="data/tmp/spark"
 readonly out_dir="derived/ptvs_test2"
@@ -27,24 +26,22 @@ readonly hail_script="utils/hail_export.py"
 
 # input path
 readonly chr=${SGE_TASK_ID}
-readonly in_phased="${in_dir_phased}/ukb_wes_200k_phased_chr${chr}.1of1.vcf.gz"
-readonly in_unphased="${in_dir_unphased}/ukb_wes_200k_filtered_chr${chr}.mt"
-readonly vep="${vep_dir}/ukb_wes_200k_full_vep_chr${chr}.vcf"
+readonly in_phased="${in_dir}/ukb_wes_200k_annotated_chr${chr}.mt"
+readonly in_unphased="${in_dir}/ukb_wes_200k_annotated_chr${chr}_singletons.mt"
 
 # output path
-readonly out_prefix="${out_dir}/ukb_wes_200k_phased_all_maf002_no_missing_filter_test_chr${chr}"
+readonly out_prefix="${out_dir}/ukb_wes_200k_maf002_miss005_ptv_chr${chr}"
 readonly out="${out_prefix}.mt"
 
 # run hail
 set_up_hail
-set_up_vep
 set_up_pythonpath
 mkdir -p ${out_dir}
 python3 "${hail_script}" \
     --chrom ${chr} \
     --input_phased_path ${in_phased}\
     --input_unphased_path ${in_unphased} \
-    --input_phased_type "vcf" \
+    --input_phased_type "mt" \
     --input_unphased_type "mt" \
     --vep_filter "ptv" \
     --maf_max 0.02 \
