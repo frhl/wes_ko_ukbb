3807ba3d2bcbcfb291e9a6ad5732d74d1c04f759
frhl <frederik.heymann@gmail.com>
2021-10-06 09:50:47 +0100

    updated shell script with genetic europeans

--- scripts/03_create_grm.sh
+++ scripts/03_create_grm.sh
@@ -2,10 +2,10 @@
 #
 # Create sparse genetic relatedness matrix using genotyped data.
 #
-#$ -N step0_saige
+#$ -N create_grm
 #$ -wd /well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb
-#$ -o logs/step0_saige.log
-#$ -e logs/step0_saige.errors.log
+#$ -o logs/create_grm.log
+#$ -e logs/create_grm.errors.log
 #$ -P lindgren.prjc
 #$ -pe shmem 10
 #$ -q long.qc
@@ -18,7 +18,7 @@ source utils/hail_utils.sh
 
 # paths for hail script
 readonly out_dir="data/saige/grm/input/eur_ukbb"
-readonly out_prefix="${out_dir}/ukb_imp_eur_wes_chr1_22_sparse_markers"
+readonly out_prefix="${out_dir}/ukb_eur_wes_sparse_markers_autosomes"
 readonly hail_script="utils/subscripts/create_grm_input.py"
 readonly spark_dir="data/tmp/spark"
 readonly threads=$(( ${NSLOTS}-1 ))
@@ -32,11 +32,11 @@ set_up_hail
 set_up_pythonpath
 mkdir -p ${out_dir}
 python3 "${hail_script}" \
-  chroms ${chroms} \
-  out_prefix ${out_prefix} \
-  subset_markers_by_kinship \
-  subset_samples_by_wes200k \
-  subset_samples_by_ukbb_eur
+  --chroms ${chroms} \
+  --out_prefix ${out_prefix} \
+  --subset_markers_by_kinship \
+  --subset_samples_by_wes200k \
+  --subset_samples_by_ukbb_eur
 
 print_update "Successfully combined .bgen files for GRM input." "${SECONDS}"
 
@@ -47,7 +47,7 @@ print_update "Generating GRM from plink files.. "
 Rscript "${createSparseGRM}" \
   --plinkFile=${out_prefix} \
   --nThreads=4 \
-  --outputPrefix=${out_prefix}	\
-  --numRandomMarkerforSparseKin=1000	\
+  --outputPrefix=${out_prefix} \
+  --numRandomMarkerforSparseKin=1000 \
   --relatednessCutoff=0.125
 
