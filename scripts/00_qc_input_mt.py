3585ac6a27a529e6fadbe4b2c140301c326b1a68
frhl <frederik.heymann@gmail.com>
2021-10-18 21:05:06 +0100

    added new QC script to assess difference between phased and non-phased data

--- scripts/00_qc_input_mt.py
+++ scripts/00_qc_input_mt.py
@@ -21,7 +21,9 @@ def main(args):
     out_prefix = args.out_prefix
     out_type   = args.out_type
     vep_path   = args.vep_path
-    
+    final_variant_list = args.final_variant_list
+    final_sample_list = args.final_sample_list    
+
     # setup flags
     hail_init.hail_bmrc_init_local('logs/hail/hail_format.log', 'GRCh38')
     hl._set_flags(no_whole_stage_codegen='1') # from zulip
@@ -30,15 +32,22 @@ def main(args):
     mt1 = qc.get_table(input_path=input_phased_path, input_type=input_phased_type) # 12788
     mt2 = qc.get_table(input_path=input_unphased_path, input_type=input_unphased_type) # 11867 (for singletons)
 
+    # filter by final samples
+    if final_sample_list:
+        ht_final_samples = hl.import_table(final_sample_list, no_header=True, key='f0')
+        mt1 = mt1.filter_cols(hl.is_defined(ht_final_samples[mt1.col_key]))
+        mt2 = mt2.filter_cols(hl.is_defined(ht_final_samples[mt2.col_key]))
+   
+    # filter by final variants
+    if final_variant_list:
+        ht_final_variants = hl.import_table(final_variant_list, types={'locus':hl.tlocus(reference_genome='GRCh38'), 'alleles':hl.tarray(hl.tstr)})
+        ht_final_variants = ht_final_variants.key_by(ht_final_variants.locus, ht_final_variants.alleles)
+        mt1 = mt1.filter_rows(hl.is_defined(ht_final_variants[mt1.row_key]))
+        mt2 = mt2.filter_rows(hl.is_defined(ht_final_variants[mt2.row_key]))
+
     # Add QC fields that has been removed during phasing to mt1
     mt1 = mt1.annotate_entries(DP = mt2[(mt1.locus, mt1.alleles), mt1.s].DP)
     mt1 = mt1.annotate_entries(GQ = mt2[(mt1.locus, mt1.alleles), mt1.s].GQ)
-
-    # note: need to translate ids to combine later!
-    mt1 = qc.annotate_european(mt1)
-    mt2 = qc.annotate_european(mt2)
-
-    ### Variant filtering/annotations
 
     # Run VEP + gnomAD variant annotations
     mt1 = process_consequences(hl.vep(mt1, "utils/configs/vep_env.json"))
@@ -63,12 +72,24 @@ def main(args):
     # By default add snpid id annotation
     mt1 = qc.annotate_snpid(mt1)
     mt2 = qc.annotate_snpid(mt2)   
-    
-    
-    # export files
-    mt1.write(out_prefix + "_phased.mt")
-    mt2.write(out_prefix + "_unphased.mt")
 
+    # write out variant stats
+    mt1 = hl.variant_qc(mt1, name='variant_qc')
+    ht1_rows_filter = mt1.rows()
+    ht1_rows_filter.select().write(out_prefix + "_variants_phased.tsv.bgz", overwrite=True)
+ 
+    mt2 = hl.variant_qc(mt2, name='variant_qc')
+    ht2_rows_filter = mt2.rows()
+    ht2_rows_filter.select().write(out_prefix + "_variants_unphased.tsv.bgz", overwrite=True)
+    
+    # write out samples stats
+    mt1 = mt.annotate_cols(gq = hl.agg.stats(mt1.GQ), dp = hl.agg.stats(mt1.DP))
+    mt1 = hl.sample_qc(mt1, name='sample_qc')
+    mt1.cols().select('sample_qc', 'gq', 'dp').flatten().export(output=out_prefix + "_samples_phased.tsv.bgz")
+ 
+    mt2 = mt.annotate_cols(gq = hl.agg.stats(mt2.GQ), dp = hl.agg.stats(mt2.DP))
+    mt2 = hl.sample_qc(mt2, name='sample_qc')
+    mt2.cols().select('sample_qc', 'gq', 'dp').flatten().export(output=out_prefix + "_samples_unphased.tsv.bgz")
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser()
@@ -81,7 +102,9 @@ if __name__=='__main__':
     parser.add_argument('--out_type', default=None, help='Type of output dataset (options: mt, vcf, plink)')
     parser.add_argument('--chrom', default=None, help='Chromosome to be used')
     parser.add_argument('--vep_path', default=None, help='path to a .vcf file containing annotated entries by locus and alleles')   
- 
+    parser.add_argument('--final_sample_list', default=None, help='Path to hail table with final samples to be included')
+    parser.add_argument('--final_variant_list', default=None, help='Path to hail table with final variants to be included')     
+
     args = parser.parse_args()
 
     main(args)
--- scripts/00_qc_input_mt.sh
+++ scripts/00_qc_input_mt.sh
@@ -1,31 +1,30 @@
 #!/usr/bin/env bash
 #
-#$ -N hail_format
+#$ -N qc_input_mt
 #$ -wd /well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb
-#$ -o logs/create_mt.log
-#$ -e logs/create_mt.errors.log
+#$ -o logs/qc_input_mt.log
+#$ -e logs/qc_input_mt.errors.log
 #$ -P lindgren.prjc
 #$ -pe shmem 10
 #$ -q short.qc@@short.hge
-#$ -t 6
+#$ -t 21
 
-set -o errexit
-set -o nounset
 
 source utils/qsub_utils.sh
 source utils/hail_utils.sh
 
 # directories
-
 readonly in_dir_phased="/well/lindgren/UKBIOBANK/nbaya/wes_200k/phase_ukb_wes/data/phased/non_singleton"
 #readonly in_dir_phased="data/phased"
 readonly in_dir_unphased="data/unphased/post-qc"
 readonly vep_dir="data/vep/full"
+
 readonly spark_dir="data/tmp/spark"
-readonly out_dir="data/mt"
+readonly out_dir="data/qc"
+
 
 # hail script
-readonly hail_script="utils/subscripts/hail_format.py"
+readonly hail_script="scripts/00_qc_input_mt.py"
 
 # input path
 readonly chr=$( get_chr ${SGE_TASK_ID} ) 
@@ -34,29 +33,26 @@ readonly in_phased="${in_dir_phased}/ukb_wes_phased_non_singleton_chr${chr}-24xl
 readonly in_unphased="${in_dir_unphased}/ukb_wes_200k_filtered_chr${chr}.mt"
 readonly vep="${vep_dir}/ukb_wes_200k_full_vep_chr${chr}.vcf"
 
+readonly final_sample_list='/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc/data/samples/09_final_qc.keep.sample_list'
+readonly final_variant_list='/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc/data/variants/08_final_qc.keep.variant_list'
+
 # output path
-readonly out_prefix="${out_dir}/ukb_wes_200k_annotated_chr${chr}"
-readonly out="${out_prefix}.mt"
+readonly out_prefix="${out_dir}/ukb_wes_200k_chr${chr}"
 
 # run hail
-mkdir -p ${out_dir}
-if [ ! -f "${out}/_SUCCESS" ]; then
-  set_up_hail
-  set_up_vep
-  set_up_pythonpath  
-  python3 "${hail_script}" \
+set_up_hail
+set_up_vep
+set_up_pythonpath  
+python3 "${hail_script}" \
      --chrom ${chr} \
      --input_phased_path ${in_phased}\
      --input_unphased_path ${in_unphased} \
      --input_phased_type "vcf" \
      --input_unphased_type "mt" \
      --vep_path ${vep} \
+     --final_sample_list ${final_sample_list} \
+     --final_variant_list ${final_variant_list}\
      --out_prefix ${out_prefix}
-else
-  print_update "file ${out} already exists. Skipping!"
-fi
-
-
 
 print_update "Finished running HAIL for chr${chr}" "${SECONDS}"
 
