de76c599770adba3454eda0dd0a8e704d392120e
frhl <frederik.heymann@gmail.com>
2021-10-24 17:10:20 +0100

    added plotting functionality for genenass

---
+++ scripts/QC/09_plot_genebass.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+#
+#$ -N plot_genebass
+#$ -wd /well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb
+#$ -o logs/plot_genebass.log
+#$ -e logs/plot_genebass.errors.log
+#$ -P lindgren.prjc
+#$ -pe shmem 4
+#$ -q short.qc@@short.hge
+
+
+source utils/qsub_utils.sh
+source utils/bash_utils.sh
+source utils/hail_utils.sh
+
+# directories
+readonly in_dir="data/variants"
+readonly out_dir="derived/variants"
+
+# hail script
+readonly rcode="scripts/QC/09_plot_genebass.R"
+
+# output path
+readonly out_prefix="${out_dir}/ukb_wes_200k_genebass_AF"
+
+# run 
+set_up_RSAIGE
+mkdir -p ${out_dir}
+Rscript "${rcode}" \
+     --in_dir ${in_dir} \
+     --in_pattern 'genebass' \
+     --out_prefix ${out_prefix}
+
+
