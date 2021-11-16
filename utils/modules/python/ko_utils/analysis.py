d051aff033b1994f490ff0da1a17f4fc6ee1b586
frhl <frederik.heymann@gmail.com>
2021-11-16 15:24:43 +0000

    added gnomAD/Duncan Functions

--- utils/modules/python/ko_utils/analysis.py
+++ utils/modules/python/ko_utils/analysis.py
@@ -345,9 +345,7 @@ def create_gene_map_ht(ht, check_gene_contigs=False):
     ht = ht.explode(ht.vep.worst_csq_by_gene_canonical)
     ht = ht.annotate(
         variant_id=ht.locus.contig + ':' + hl.str(ht.locus.position) + '_' + ht.alleles[0] + '/' + ht.alleles[1],
-        annotation=annotation_case_builder(ht.vep.worst_csq_by_gene_canonical,
-                                           ht.vep.worst_csq_for_variant_canonical,
-                                           ht.dbnsfp)
+        annotation=annotation_case_builder(ht.vep.worst_csq_by_gene_canonical)
         )
     if check_gene_contigs:
         gene_contigs = ht.group_by(
@@ -380,5 +378,18 @@ def post_process_gene_map_ht(gene_ht):
     gene_ht = gene_ht.transmute(annotation=gene_ht.variant_groups[0], variants=hl.sorted(gene_ht.variant_groups[1]))
     gene_ht = gene_ht.key_by(start=gene_ht.interval.start)
     return gene_ht.filter(hl.len(gene_ht.variants) > 0)
+
+def count_variants(vep_ht_path, vep_vcf_path):
+    from gnomad.utils.vep import process_consequences
+    ht = hl.read_table(vep_ht_path)
+    ht = process_consequences(ht)
+    ht = ht.explode(ht.vep.worst_csq_by_gene_canonical)
+    ht = ht.annotate(
+        variant_id=ht.locus.contig + ':' + hl.str(ht.locus.position) + '_' + ht.alleles[0] + '/' + ht.alleles[1],
+        annotation=annotation_case_builder(ht.vep.worst_csq_by_gene_canonical)
+        )
+    ht = ht.filter(hl.literal({'ptv', 'ptv_LC', 'damaging_missense', 'other_missense', 'synonymous'}).contains(ht.annotation))
+    print(ht.count())
+
 
 
