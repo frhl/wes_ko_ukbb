3933fa8ea12254ab83eaab400dc7e145d787b01a
frhl <frederik.heymann@gmail.com>
2021-11-22 21:08:39 +0000

    checkpoint before revision

--- scripts/02_qc_samples.sh
+++ scripts/02_qc_samples.sh
@@ -5,9 +5,9 @@
 #$ -o logs/test_qc_samples.log
 #$ -e logs/test_qc_samples.errors.log
 #$ -P lindgren.prjc
-#$ -pe shmem 4
+#$ -pe shmem 8
 #$ -q long.qc@@long.hge
-#$ -t 1-22
+#$ -t 1,19
 
 source utils/qsub_utils.sh
 source utils/hail_utils.sh
@@ -19,8 +19,8 @@ readonly spark_dir="data/tmp/spark"
 readonly out_dir="data/qc"
 
 # hail script
-#readonly hail_script="scripts/02_qc_samples.py"
-readonly hail_script="scripts/02_qc_unphased_samples.py"
+readonly hail_script="scripts/02_qc_samples.py"
+#readonly hail_script="scripts/02_qc_unphased_samples.py"
 
 # input paths
 readonly chr=$( get_chr ${SGE_TASK_ID} ) 
@@ -33,7 +33,7 @@ readonly final_sample_list='/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc
 readonly final_variant_list='/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc/data/variants/08_final_qc.keep.variant_list'
 
 # output path
-readonly out_prefix="${out_dir}/ukb_wes_200k_unphased_chr${chr}"
+readonly out_prefix="${out_dir}/ukb_wes_200k_chr${chr}"
 
 # run hail
 mkdir -p ${out_dir}
