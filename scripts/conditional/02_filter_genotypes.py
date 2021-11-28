763904e6c3e095b6ceb63284300c0be3b02cd343
frhl <frederik.heymann@gmail.com>
2021-11-28 22:20:46 +0000

    write vcf that include common variants around certaing genes

---
+++ scripts/conditional/02_filter_genotypes.py
@@ -0,0 +1,97 @@
+import os
+import hail as hl
+import argparse
+
+from ukb_utils import hail_init
+from ukb_utils import genotypes
+from ukb_utils import samples
+from ukb_utils import variants
+from ko_utils import qc
+
+def main(args):
+
+    
+    out_prefix = args.out_prefix
+    gene_table = args.gene_table
+    final_sample_list = args.final_sample_list
+    phenotype = args.phenotype
+    padding = int(args.padding)
+
+    reference_genome = 'GRCh38'
+    hail_init.hail_bmrc_init_local('logs/hail/hail_conditional_tables.log', reference_genome)
+    hl._set_flags(no_whole_stage_codegen='1')
+
+    # load table of genes that are significant in primary analysis
+    ht = hl.import_table(gene_table, force_bgz = True, delimiter = '\t', missing = '',
+                     types={'contig': hl.tstr,'start': hl.tint, 'end': hl.tint, 'contig_length': hl.tint}) 
+    ht = ht.annotate(contig = hl.delimit(['chr',ht.contig],''))
+    
+    # filter phenotype and damaging mutations
+    ht = ht.filter(ht.phenotype == phenotype)
+    #ht = ht.filter(ht.mutation != "synonymous")
+    n_phenos = ht.count()
+    if n_phenos == 0:
+        raise ValueError(str(phenotype) + " is not in gene table") 
+
+    # annotate ranges and ensure that they are within chromosome regions 
+    ht = ht.annotate(ranges = hl.array([ht.start,ht.end]))
+    ht = ht.annotate(
+        end = hl.max(ht.ranges),
+        start = hl.min(ht.ranges))
+    ht = ht.annotate(
+        end_with_padding = hl.min(hl.array([ht.end + padding, ht.contig_length])),
+        start_with_padding = hl.max(hl.array([ht.start - padding, 1])))
+    ht = ht.drop(ht.ranges)
+
+    ht = ht.annotate(start_valid = hl.is_valid_locus(ht.contig, ht.start, reference_genome))
+    ht = ht.annotate(end_valid = hl.is_valid_locus(ht.contig, ht.end, reference_genome))
+    ht = ht.annotate(valid_intervals = ht.start_valid & ht.end_valid)
+    
+    defined_coords = hl.is_defined(ht.valid_intervals)
+    n_drop = ht.filter(~defined_coords).count()
+    if n_drop > 0:
+            print(f'Dropping {n_drop} undefined start/end genomic coordinates.')
+    ht = ht.filter(defined_coords)
+    invalid_intervals = ht.filter(~ht.valid_intervals).count()
+    assert invalid_intervals == 0, 'Some of the supplied intervals are outside current chromosome contig'
+    ht = ht.annotate(intervals = hl.locus_interval(ht.contig, ht.start, ht.end, True, True, reference_genome=reference_genome))
+
+    # get genotypes
+    chromosomes = [x.replace('chr','') for x in list(set(ht.contig.collect()))]
+    mt = genotypes.get_ukb_genotypes_bed(chromosomes)
+    mt = mt.annotate_rows(**{'info': hl.agg.call_stats(mt.GT, mt.alleles)})
+
+    # filter samples
+    if final_sample_list:
+         ht_final_samples = hl.import_table(final_sample_list, no_header=True, key='f0',delimiter = ',')
+         mt = mt.filter_cols(hl.is_defined(ht_final_samples[mt.col_key]))
+
+    # filter variants based on missingness
+    mt = qc.filter_min_missing(mt, 0.10)
+
+    # filter to common variants
+    mt = mt.annotate_rows(info = mt.info.annotate(AC = mt.info.AC[1]))
+    mt = mt.annotate_rows(info = mt.info.annotate(AF = mt.info.AF[1]))
+    mt = qc.filter_min_maf(mt, 0.01)
+
+    # perform liftover to GRCh38
+    mt = variants.liftover(mt)
+
+    # filter to variants in the the genomic regions across all chromosomes
+    mt = hl.filter_intervals(mt, ht.intervals.collect())
+    n = mt.count()
+    print(f'filtered genotypes to {n} variants/samples. Writing to VCF..')
+    hl.export_vcf(mt, out_prefix + 'tsv.bgz')
+
+if __name__=='__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--padding', default=0, help='How much extra padding should be included around genes (upstream/downsream)?')
+    parser.add_argument('--final_sample_list', default=None, help='Path to HailTable that contains the final samples included in the analysis.')
+    parser.add_argument('--gene_table', default=None, help='Path to HailTable that contains the significant genes from primary analysis.')
+    parser.add_argument('--phenotype', default=None, help='Path to phenotype table that also includes sex.')
+    parser.add_argument('--out_prefix', default=None, help='Path prefix for output dataset (plink format)')
+    args = parser.parse_args()
+
+    main(args)
+
---
+++ scripts/conditional/02_filter_genotypes.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+# extract genotypes in regions near genes that are significant in primary analysis
+#
+#$ -N filter_genotypes
+#$ -wd /well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb
+#$ -o logs/filter_genotypes.log
+#$ -e logs/filter_genotypes.errors.log
+#$ -P lindgren.prjc
+#$ -pe shmem 4
+#$ -q short.qc
+#$ -t 3
+
+source utils/bash_utils.sh
+source utils/hail_utils.sh
+
+readonly spark_dir="data/tmp/spark"
+readonly pheno_dir="data/phenotypes"
+readonly gene_dir="derived/tables/gene_intervals"
+readonly out_dir="data/conditional/common"
+
+readonly final_sample_list='/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc/data/samples/09_final_qc.keep.sample_list'
+readonly gene_table="${gene_dir}/211111_wes200k_saige_wes_saige_sig_genes_intervals.txt"
+readonly pheno_list="${pheno_dir}/UKBB_WES200k_binary_phenotypes_header.txt"
+readonly hail_script="scripts/conditional/02_filter_genotypes.py"
+
+readonly index=${SGE_TASK_ID}
+readonly phenotype=$( cut -f${index} ${pheno_list} )
+
+readonly out_prefix="${out_dir}/211111_ukb_wes200k_sig_genes_genotype_regions"
+
+set_up_hail
+set_up_pythonpath_legacy
+mkdir -p ${out_dir}
+python3 "${hail_script}" \
+   --phenotype ${phenotype} \
+   --padding 500000 \
+   --gene_table ${gene_table} \
+   --final_sample_list ${final_sample_list} \
+   --out_prefix ${out_prefix} 
+
+print_update "Hail finished writing."
+
+
