7157f51af1b0338463a6d5e85b0b89f1130501b9
frhl <frederik.heymann@gmail.com>
2021-11-29 18:25:00 +0000

    started script for SPA conditional loop

---
+++ scripts/conditional/03_spa_conditional.sh
@@ -0,0 +1,97 @@
+#!/usr/bin/env bash
+#
+#
+#$ -N spa_conditional
+#$ -wd /well/lindgren/UKBIOBANK/flassen/projects/KO/wes_ko_ukbb
+#$ -o logs/spa_conditional.log
+#$ -e logs/spa_conditional.errors.log
+#$ -P lindgren.prjc
+#$ -pe shmem 1
+#$ -q lindgren.qe
+
+source utils/bash_utils.sh
+source utils/hail_utils.sh
+
+# arguments
+readonly phenotype=${1?Error: Missing arg1 (phenotype)}
+readonly in_vcf=${2?Error: Missing arg2 (in_vcf)}
+readonly in_csi=${3?Error: Missing arg2 (in_csi)}
+readonly in_gmat=${4?Error: Missing arg3 (in_gmat)} 
+readonly in_var=${5?Error: Missing arg4 (in_var)} 
+readonly out_prefix=${6?Error: Missing arg5 (path prefix for saige output)}
+readonly chr=${SGE_TASK_ID}
+
+# subsitute each chromosome
+readonly vcf=$(echo ${in_vcf} | sed -e "s/CHR/${chr}/g")
+readonly csi=$(echo ${in_csi} | sed -e "s/CHR/${chr}/g")
+
+# SAIGE paths
+#readonly threads=$(( ${NSLOTS}-1 ))
+#readonly createSparseGRM="/well/lindgren/flassen/software/dev/SAIGE/extdata/createSparseGRM.R"
+#readonly step1_fitNULLGLMM="/well/lindgren/flassen/software/dev/SAIGE/extdata/step1_fitNULLGLMM.R"
+readonly step2_SPAtests="/well/lindgren/flassen/software/dev/SAIGE/extdata/step2_SPAtests.R"
+
+# out prefix
+readonly out="${out_prefix}_chr${chr}"
+
+spa_test() {
+   SECONDS=0
+   print_update "SAIGE-SPA: Writing '${out}' using '${vcf}'"
+   set -x
+   Rscript "${step2_SPAtests}"  \
+     --vcfFile=${vcf} \
+     --vcfFileIndex=${csi} \
+     --vcfField="DS" \
+     --chrom="chr${chr}" \
+     --minMAF=0.0000001 \
+     --minMAC=3 \
+     --GMMATmodelFile=${in_gmat} \
+     --varianceRatioFile=${in_var} \
+     --SAIGEOutputFile=${out} \
+     --numLinesOutput=2 \
+     --IsOutputAFinCaseCtrl=TRUE \
+     --IsOutputNinCaseCtrl=TRUE \
+     --IsOutputHetHomCountsinCaseCtrl=TRUE \
+     --LOCO=FALSE \
+     --condition "${1}"
+   set +x
+   duration=${SECONDS}
+   print_update "Finished SPA test for variants ${1} with ${out} at ${duration}"
+}
+
+get_spa_marker() {
+  
+  markers=$( cat ${out} | \
+    awk -v P="${P_cutoff}" '$13 < P' | \
+    sort -k 13,13 | \
+    cut -d" " -f3 | \
+    head -n1)
+    
+  #tr "\n" "," | sed '$s/,$//'
+
+ echo $markers
+
+}
+
+
+set_up_RSAIGE
+ITER=0
+MAX_ITER=10
+
+
+markers=get_spa_markers
+
+
+while [ ${n_variants} -gt 0 ]; do 
+  spa_test ${variants}
+  ITER=`expr $ITER + 1`
+  if [ $ITER -gt $MAX_ITER ]; then
+    break
+  fi
+done 
+
+
+
+
+
+
