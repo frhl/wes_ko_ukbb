2d5648098ae60e4676317e99fb6e7c451282174f
frhl <frederik.heymann@gmail.com>
2021-12-03 15:27:48 +0000

    add chr to ${chr}

--- scripts/QC/10_phase_with_ref.sh
+++ scripts/QC/10_phase_with_ref.sh
@@ -13,19 +13,21 @@ source utils/qsub_utils.sh
 
 readonly in_dir="data/unphased/post-qc/"
 readonly out_dir="data/phased/test-phasing"
+readonly ref_dir="/well/lindgren/flassen/ressources/panels/liftover_reference_panel/data/liftover"
 
 readonly chr="${SGE_TASK_ID}"
 readonly in_file="${in_dir}/ukb_wes_200k_filtered_chr${chr}.vcf.bgz"
 readonly out_file="${out_dir}/ukb_wes_200k_refphased_chr${chr}.vcf.gz"
 
-readonly ref=""
+readonly ref="${ref_dir}/ALL.chr${chr}.phase3_shapeit2_mvncall_integrated_v5.20130502.genotypes.vcf.bgz"
 readonly gmap="/well/lindgren/flassen/software/SHAPEIT4/b38.gmap/chr${chr}.b38.gmap.gz"
 
 mkdir -p ${out_dir}
 module load SHAPEIT4/4.2.0-foss-2019b
-shapeit4 --input ${OUT_ROOT}${IN_VCF} \
+shapeit4.2 \
+  --input ${in_file} \
   --map ${gmap} \
-  --region ${chr} \
+  --region "chr${chr}" \
   --thread 20 \
   --reference ${ref} \
   --output ${out_file}
