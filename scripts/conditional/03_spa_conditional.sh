aced89c2694534a0f0bb1ade6bd9c94518120742
frhl <frederik.heymann@gmail.com>
2021-12-03 12:06:00 +0000

    checkpoint

--- scripts/conditional/03_spa_conditional.sh
+++ scripts/conditional/03_spa_conditional.sh
@@ -28,103 +28,89 @@ readonly in_gmat="${step1_dir}/ukb_wes_200k_${phenotype}.rda"
 readonly in_var="${step1_dir}/ukb_wes_200k_${phenotype}.varianceRatio.txt"
 
 # in/out files 
-readonly vcf="${in_dir}/211111_genetic_intervals_${phenotype}.vcf.bgz"
-readonly csi="${vcf}.csi"
-readonly out="${out_dir}/211111_significant_variants_${phenotype}.txt"
-readonly out_prefix="${out_dir}/211111_significant_variants_${phenotype}"
-readonly out_tmp="${out}.tmp"
-
-# loop params
-readonly P_cutoff=0.0001
+#readonly vcf="${in_dir}/211111_genetic_intervals_${phenotype}.vcf.bgz"
+#readonly out_prefix="${out_dir}/211111_significant_variants_${phenotype}"
 readonly step2_SPAtests="/well/lindgren/flassen/software/dev/SAIGE/extdata/step2_SPAtests.R"
 
-#spa_test() {
-#  set -x
-#  Rscript "${step2_SPAtests}"  \
-#    --vcfFile=${vcf} \
-#    --vcfFileIndex=${csi} \
-#    --vcfField="GT" \
-#    --chrom="chr${chr}" \
-#    --minMAF=0.0000001 \
-#    --minMAC=1 \
-#    --GMMATmodelFile=${in_gmat} \
-#    --varianceRatioFile=${in_var} \
-#    --SAIGEOutputFile=${prefix_chr} \
-#    --numLinesOutput=2 \
-#    --IsOutputAFinCaseCtrl=TRUE \
-#    --IsOutputNinCaseCtrl=TRUE \
-#    --IsOutputHetHomCountsinCaseCtrl=TRUE \
-#    --LOCO=FALSE \
-#    ${1:+--condition "$1"}
-#  set +x
-#}
+# loop params
 
-spa_chromosome_loop() {
-   printf 'Markers %s\n' "$1"
-   for chr in {7..8}; do
-      local prefix_chr="${prefix_iter}_chr${chr}"
+spa_loop() {
+   printf 'VCF %s' "$1"
+   printf 'Markers %s' "$2"
+   printf 'Prefix %s' "$3"
+   for chr in {1..22}; do
+      local prefix_chr="${3}_chr${chr}"
       set -x
       Rscript "${step2_SPAtests}"  \
-        --vcfFile=${vcf} \
-        --vcfFileIndex=${csi} \
+        --vcfFile=${1} \
+        --vcfFileIndex="${1}.csi" \
         --vcfField="GT" \
         --chrom="chr${chr}" \
         --minMAF=0.0000001 \
         --minMAC=1 \
         --GMMATmodelFile=${in_gmat} \
         --varianceRatioFile=${in_var} \
-        --SAIGEOutputFile=${prefix_chr} \
+        --SAIGEOutputFile=${3} \
         --numLinesOutput=2 \
         --IsOutputAFinCaseCtrl=TRUE \
         --IsOutputNinCaseCtrl=TRUE \
         --IsOutputHetHomCountsinCaseCtrl=TRUE \
         --LOCO=FALSE \
-        ${1:+--condition "$1"}
+        ${2:+--condition "$2"}
       set +x
    done 
 }
 
-get_sig_marker() {
-  echo $( cat "${prefix_iter}.txt" | \
-    awk -v P="${P_cutoff}" '$13 < P' | \
-    sort -k 13,13 | \
-    cut -d" " -f3 | \
-    head -n1)
+spa_conditional() {
+  vcf=${1}
+  out_prefix=${2}
+  local i=0
+  local max=5
+  local marker_list=""
+  while [ $i -lt $max ]; do
+      
+      printf 'Iteration: %s' "$i"
+      printf 'Markers: %s' "$marker_list"
+      true $(( i++ ))
+      
+      local prefix_iter="${out_prefix}_i${i}"
+      local prefix_iter_chr="prefix_iter_chr"
+      local markers_combined="${prefix_iter}.txt"
+
+      spa_loop ${vcf} ${marker_list} ${prefix_iter}
+      cat ${prefix_iter_chr}* > ${markers_combined}
+      rm  ${prefix_iter_chr}*
+      
+      marker=$( cat "${markers_combined}" | \
+        awk -v P="${P_cutoff}" '$13 < P' | \
+        sort -k 13,13 | \
+        cut -d" " -f3 | \
+        head -n1)
+
+      if [ -z ${marker} ]; then
+          break    
+      fi    
+      
+      if [ -z ${marker_list} ]; then
+          markers=${marker}
+      else
+          marker_list+=",${marker}"        
+      fi        
+      
+  done
 }
 
-
-
-
-i=0
-max=2
-marker_list=""
+# loop params
+readonly P_cutoff=0.0001
 set_up_RSAIGE
-while [ $i -lt $max ]
-do
-    echo "iteration: $i"
-    true $(( i++ ))
-    
-    prefix_iter="${out_prefix}_i${i}"
-    spa_chromosome_loop ${marker_list}
-    #cat "${prefix_iter}_chr"* > "${prefix_iter}.txt"
-    #rm  "${prefix_iter}_chr"*
-    marker=$(get_sig_marker)
+mkdir -p ${out_dir}
 
-    echo "echo ${marker}"
-    if [ ! -z ${marker} ]; then
-        if [ -z ${marker_list} ]; then
-            markers=${marker}
-        else
-            marker_list+=",${marker}"        
-        fi        
-    else
-        echo 'ending loop..'
-        ${marker_list} > ${out}
-        break    
-    fi    
-    echo "markers: ${marker_list}"
+annotation="synonymous"
+vcf="${in_dir}/211111_intervals_${annotation}_${phenotype}.vcf.bgz"
+out_prefix="${out_dir}/211111_spa_conditional_${annotation}_${phenotype}"
+spa_conditional ${vcf} ${out_prefix}
 
-done
+
 
 
 
