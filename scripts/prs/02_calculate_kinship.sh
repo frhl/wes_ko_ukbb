2d106625c2723300d520333a70f93f2c98a233b3
frhl <frederik.heymann@gmail.com>
2022-01-19 15:31:15 +0000

    strange error

--- scripts/prs/02_calculate_kinship.sh
+++ scripts/prs/02_calculate_kinship.sh
@@ -12,24 +12,43 @@
 
 
 readonly in_dir="data/saige/grm/input"
-readonly out_dir="data/prs/kinship"
+readonly out_dir="data/prs/kinship/matrix"
 
 readonly ldak_dir="/well/lindgren/flassen/software/ldak"
 readonly ldak="${ldak_dir}/ldak5.2.linux"
 
 readonly in_file="${in_dir}/211102_long_ukb_wes_200k_sparse_autosomes"
-readonly out="${out_dir}/kins"
+readonly out_thin="${out_dir}/thin"
+readonly out_kins="${out_dir}/kins"
 
 readonly sample_list='/well/lindgren/UKBIOBANK/dpalmer/wes_200k/ukb_wes_qc/data/samples/09_final_qc.keep.sample_list'
 
 mkdir -p ${out_dir}
 
-set -x 
 
+# thinning predictors
+set -x 
 ${ldak} \
-  --cut-kins "${out}" \
-  --partition-length "500000" \
-  --bfile "${in_file}"
+  --thin "${out_thin}" \
+  --bfile "${in_file}" \
+  --window-prune .98 \
+  --window-kb 100 \
+set +x
+
+# generate weights file
+awk < "${out_thin}.in" '{print $1, 1}' > "${out_thin}.weights"
+
+
+#${ldak} \
+#  --calc-kins "${out}" \
+#  --bfile "${in_file}" \
+#  -ignore-weights YES \
+#  --power -1
+
+#${ldak} \
+#  --cut-kins "${out}" \
+#  --partition-length "500000" \
+#  --bfile "${in_file}"
 
 #bash "${ldak}" \
 #  --calc-kins ${out} \
@@ -40,7 +59,6 @@ ${ldak} \
 #bash "${ldak}" \
 #  --join-kins ${out_dir}
 
-set +x
 
 
 
