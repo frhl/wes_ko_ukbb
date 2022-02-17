b2c131d220a5af38128566766e74cc032aa57edc
frhl <frederik.heymann@gmail.com>
2022-02-17 10:35:06 +0000

    now correctly adds header

--- scripts/prs/_summary_statistics_merge.sh
+++ scripts/prs/_summary_statistics_merge.sh
@@ -20,9 +20,11 @@ readonly n=$(ls -l "${out_dir}" | grep -E "${files}" | wc -l)
 readonly N=21
 
 if (( $(echo "$n > $N" | bc -l) )); then
-  zcat ${file}* | tail -n+2 > "${out_without_gz}"
   for chr in {1..22}; do
      file=$(echo ${prefix} | sed -e "s/CHR/${chr}/g")
+     if [ "${chr}" == "1" ]; then
+        zcat "${file}.txt.gz" | head -n 1  >> "${out_without_gz}"
+     fi
      zcat "${file}.txt.gz" | tail -n +2  >> "${out_without_gz}"
   done
   gzip "${out_without_gz}"
