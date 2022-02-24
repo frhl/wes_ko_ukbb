c9893d7d7209a8665da572c00a219d474c969476
frhl <frederik.heymann@gmail.com>
2022-02-24 13:08:48 +0000

    added functionality for aggregating sers

--- scripts/phasing/ser.R
+++ scripts/phasing/ser.R
@@ -1,41 +1,43 @@
-library(data.table)
-library(argparse)
+devtools::load_all("utils/modules/R/phasingtools")
 
+main <- function(args){
+
+  print(args)
+  stopifnot(dir.exists(args$in_dir))
+  stopifnot(dir.exists(args$out_prefix))
+  
+  files <- list.files(args$in_dir, pattern = args$in_ext, full.names = TRUE)
+  if (!is.null(grep)) files <- files[grepl(args$grep, basename(files))]
+  stopifnot(length(files) > 0)
+
+  # simple analysis of non-hacked BCFtools
+  if (!is.null(args$simple)){
+  
+    M <- do.call(rbind, lapply(files, function(f){
+        d <- fread(f) 
+        d <- summarize_bcftools_trio_stats(d)  
+        d$chr <- str_extract(f, 'chr[0-9]+')
+        return(d)
+    }))
+
+    return(M)
+  }
+
+  # Site-specific analysis
+  
+
+
+}
+
+# add arguments
 parser <- ArgumentParser()
-parser$add_argument("--switch-error-file", default=NULL, help = "Input file generated with bcftools +trio-switch-errors")
-parser$add_argument("--variant-file", default=NULL, help = "File containing the variants in the original file used for SER")
-parser$add_argument("--outfile", default=NULL, help = "Destioning to output file")
+parser$add_argument("--in_dir", default=NULL, required = TRUE, help = "Path to .bed file used for calculating LD panel")
+parser$add_argument("--in_ext", default='*.txt', help = "Path to .bed file used for calculating LD panel")
+parser$add_argument("--grep", default=NULL, help = "Path to .bed file used for calculating LD panel")
+parser$add_argument("--simple", default=NULL, action = 'store_true', help = "Path to .bed file used for calculating LD panel")
+parser$add_argument("--out_prefix", default=NULL, required = TRUE, help = "Where should the results be written?")
 args <- parser$parse_args()
 
-file_switch_error <- args$switch_error_file
-file_variants <- args$variant_file
-outfile <- args$outfile
-
-stopifnot(!is.null(file_switch_error))
-stopifnot(!is.null(file_variants))
-stopifnot(!is.null(outfile))
-
-# deal with switch errors and aggregate
-cmd <- paste("grep -v '^#'", file_switch_error)
-dt <- fread(cmd = cmd)
-dt <- dt[order(dt$Pos, dt$TRIO),]
-
-colnames(dt) <- c('trio_id','index', 'switch', 'switches')
-dt$index <- dt$index + 1 # go from 0 to 1-index
-
-# deal with variants and combine with swtich erros
-vcf <- fread(file_variants)
-if (ncol(vcf) == 10) colnames(vcf) <- c('ID', 'CHR', 'POS', 'REF', 'ALT', 'MAF', 'AF', 'AC', 'AN', 'HWE')
-if (ncol(vcf) == 8) colnames(vcf) <- c('ID', 'CHR', 'POS', 'REF', 'ALT', 'MAF', 'AF', 'HWE')
-if (ncol(vcf) == 7) colnames(vcf) <- c('ID', 'CHR', 'POS', 'REF', 'ALT', 'MAF', 'AF')
-if (ncol(vcf) == 6) colnames(vcf) <- c('ID', 'CHR', 'POS', 'REF', 'ALT', 'MAF')
-ndt <- cbind(vcf[dt$index,], dt)
-
-# write out
-stopifnot(nrow(ndt) == nrow(dt))
-fwrite(ndt, outfile, sep = '\t')
-
-# 
-
+main(args)
 
 
