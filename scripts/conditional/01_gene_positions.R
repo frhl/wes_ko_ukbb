5088e1f872872bf58e8e00a8a37667039f6f8b71
frhl <frederik.heymann@gmail.com>
2022-03-10 20:08:34 +0000

    updated framework for extracting gene intervals that are significant

---
+++ scripts/conditional/01_gene_positions.R
@@ -0,0 +1,43 @@
+
+library(argparse)
+library(data.table)
+
+main <- function(args){
+
+    spa <- fread(args$in_spa_file)
+    positions <- fread(args$coordinates)
+    mrg <- merge(spa, positions, all.x = TRUE, by.x = 'MarkerID', by.y = 'ensembl_gene_id')
+        
+    mrg$fdr <- stats::p.adjust(mrg$p.value, method = 'fdr')
+    mrg <- mrg[order(mrg$fdr),]
+    mrg <- mrg[mrg$fdr < args$fdr_cutoff,]
+
+    out <- data.table(
+        gene = mrg$gene_id,
+        fdr = mrg$fdr,
+        contig = mrg$chromosome_name,
+        start = mrg$start_position - as.numeric(args$flanking_bp),
+        end = mrg$end_position + as.numeric(args$flanking_bp),
+        contig_length = mrg$length
+     )
+
+    if (nrow(out) > 0){
+        outfile = paste0(args$out_prefix, '.tsv.gz')
+        write(paste("writing to", outfile), stderr())
+        fwrite(out, outfile, sep = '\t')
+    } else {
+        write(paste("No genes signifcant at FDR <",args$fdr_cutoff),stderr())
+    }
+}
+
+parser <- ArgumentParser()
+parser$add_argument("--out_prefix", default=NULL, help = "Where should the results be written?")
+parser$add_argument("--in_spa_file", default=NULL, help = "in directory")
+parser$add_argument("--coordinates", default=NULL, help = "prefix of files to be loaded")
+parser$add_argument("--flanking_bp", default=0, help = "prefix of files to be loaded")
+parser$add_argument("--fdr_cutoff", default=0.20, help = "Path to list of phenotypes to be loaded.")
+args <- parser$parse_args()
+
+main(args)
+
+
