2c1087245084ffe5ec99ae61bd88c27a712aee7e
frhl <frederik.heymann@gmail.com>
2022-03-13 20:29:03 +0000

    now able to submit conditional job

--- scripts/conditional/03_spa_conditional.sh
+++ scripts/conditional/03_spa_conditional.sh
@@ -1,79 +1,78 @@
 #!/usr/bin/env bash
 #
-#
-#$ -N submit_spa_conditional
+#$ -N spa_conditional
 #$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
-#$ -o logs/submit_spa_conditional.log
-#$ -e logs/submit_spa_conditional.errors.log
+#$ -o logs/spa_conditional.log
+#$ -e logs/spa_conditional.errors.log
 #$ -P lindgren.prjc
 #$ -pe shmem 1
 #$ -q test.qc
-#$ -t 1-44
+#$ -t 1
 #$ -V
 
 set -o errexit
 set -o nounset
 
-readonly in_dir="data/conditional/common/filter_genotypes"
+readonly bash_script="scripts/conditional/_spa_conditional.sh"
+
+readonly in_dir="data/conditional/common/intervals"
+readonly out_dir="data/conditional/common/spa"
 readonly pheno_dir="data/phenotypes"
-readonly step1_dir="data/saige/output/combined/cts/step1"
-readonly out_dir="data/conditional/common/spa_conditional"
+readonly in_prefix="ukb_eur_wes_200k"
+readonly maf="0to5e-2"
 
-readonly pheno_list="${pheno_dir}/UKBB_WES200k_cts_phenotypes_header.txt"
-readonly index=${SGE_TASK_ID}
-readonly phenotype=$( cut -f${index} ${pheno_list} )
-
-readonly in_gmat="${step1_dir}/ukb_wes_200k_${phenotype}.rda"
-readonly in_var="${step1_dir}/ukb_wes_200k_${phenotype}.varianceRatio.txt"
-
+readonly min_mac=4
 readonly max_iter=10
-readonly P_cutoff=0.00001
-#readonly P_cutoff=5e-8
-readonly spa_script="scripts/conditional/_spa_conditional.sh"
+readonly P_cutoff=0.00001 #5e-8
 
-mkdir -p ${out_dir}
 
-submit_spa_cond_job() 
+submit_binary_analysis()
 {
-  local vcf=${1}
-  local out_prefix=${2}
-  local annotation=${3}
-  if [ -f ${vcf} ]; then
-    set -x
-    qsub -N "_spa_cond_${annotation}" \
-      -q "short.qc@@short.hge" \
-      -t "${SGE_TASK_ID}" \
-      -pe shmem 1 \
-      "${spa_script}" \
-      "${in_gmat}" \
-      "${in_var}" \
-      "${vcf}" \
-      "${out_prefix}" \
-      "${P_cutoff}" \
-      "${max_iter}"
-    set +x
-  else
-    >&2 echo "file '${1}' does not exist. Exiting." 
-  fi
+  local annotation="${1?Error: Missing arg1 (annotation)}"
+  local pheno_list="${pheno_dir}/filtered_phenotypes_binary_header.tsv"
+  local phenotype=$( sed "${SGE_TASK_ID}q;d" ${pheno_list} )
+  submit_cond_spa "${annotation}" "${phenotype}" "binary"
 }
 
+submit_cts_analysis()
+{
+  local annotation="${1?Error: Missing arg1 (annotation)}"
+  local pheno_list="${pheno_dir}/filtered_phenotypes_cts_manual.tsv"
+  local phenotype=$( sed "${SGE_TASK_ID}q;d" ${pheno_list} )
+  submit_cond_spa "${annotation}" "${phenotype}" "cts"
+}
 
-# submit scripts
-#annotation="ptv"
-#vcf="${in_dir}/211111_intervals_${annotation}_${phenotype}.vcf.bgz"
-#out_prefix="${out_dir}/211111_spa_conditional_${annotation}_${phenotype}"
-#submit_spa_cond_job ${vcf} ${out_prefix} ${annotation}
+submit_cond_spa()
+{
+  local annotation=${1?Error: Missing arg1 (consequence)}
+  local phenotype=${2?Error: Missing arg2 (phenotype)}
+  local trait=${3?Error: Missing arg3 (trait)}
 
-annotation="ptv_damaging_missense"
-vcf="${in_dir}/211111_intervals_${annotation}_${phenotype}.vcf.bgz"
-out_prefix="${out_dir}/211111_spa_conditional_${annotation}_${phenotype}"
-submit_spa_cond_job ${vcf} ${out_prefix} ${annotation}
+  local step1_dir="data/saige/output/combined/${trait}/step1"
+  local step2_dir="data/saige/output/combined/${trait}/step2/minmac${min_mac}"
+  local in_gmat="${step1_dir}/ukb_wes_200k_${phenotype}.rda"
+  local in_var="${step1_dir}/ukb_wes_200k_${phenotype}.varianceRatio.txt"
+  local in_vcf="${in_dir}/${in_prefix}_maf${maf}_${phenotype}_${annotation}.vcf.bgz"
+  local out_prefix="${out_dir}/${in_prefix}_maf${maf}_${phenotype}_${annotation}_cond"
 
-#annotation="synonymous"
-#vcf="${in_dir}/211111_intervals_${annotation}_${phenotype}.vcf.bgz"
-#out_prefix="${out_dir}/211111_TEST_P_spa_conditional_${annotation}_${phenotype}"
-#submit_spa_cond_job ${vcf} ${out_prefix} ${annotation}
+  mkdir -p ${out_dir}
+  set -x
+  qsub -N "_spa_conditional_${1}" \
+    -q "short.qc@@short.hge" \
+    -t "${SGE_TASK_ID}" \
+    -pe shmem 1 \
+    "${bash_script}" \
+    "${in_gmat}" \
+    "${in_var}" \
+    "${in_vcf}" \
+    "${out_prefix}" \
+    "${P_cutoff}" \
+    "${max_iter}" \
+    "${min_mac}"
+  set +x
+}
 
+submit_cts_analysis "pLoF_damaging_missense"
 
 
 
--- scripts/conditional/_spa_conditional.sh
+++ scripts/conditional/_spa_conditional.sh
@@ -3,50 +3,50 @@
 #
 #$ -N _spa_conditional
 #$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
-#$ -o logs/spa_conditional.log
-#$ -e logs/spa_conditional.errors.log
+#$ -o logs/_spa_conditional.log
+#$ -e logs/_spa_conditional.errors.log
 #$ -P lindgren.prjc
 #$ -pe shmem 1
 #$ -q lindgren.qe
 
+set -o errexit
+set -o nounset
+
 source utils/bash_utils.sh
 
-#readonly in_dir=${1?Error: Missing arg1 (in dir)}
-#readonly out_dir=${2?Error: Missing arg2 (out_dir)}
-#readonly pheno_dir=${3?Error: Missing arg3 (pheno_dir)}
-#readonly step1_dir=${4?Error: Missing arg4 (step1_dir)}
-#readonly pheno_list=${1?Error: Missing arg1 (pheno_dir)}
-readonly in_gmat=${1?Error: Missing arg2 (in_gmat)}
-readonly in_var=${2?Error: Missing arg3 (in_var)}
-readonly vcf=${3?Error: Missing arg4 (vcf)}
-readonly out_prefix=${4?Error: Missing arg5 (out_prefix)}
-readonly P_cutoff=${5?Error: Missing arg6 (P_cutoff)}
-readonly max_iter=${6?Error: Missing arg7 (max_iter)}
+readonly in_gmat=${1?Error: Missing arg1 (in_gmat)}
+readonly in_var=${2?Error: Missing arg2 (in_var)}
+readonly vcf=${3?Error: Missing arg3 (vcf)}
+readonly out_prefix=${4?Error: Missing arg4 (out_prefix)}
+readonly P_cutoff=${5?Error: Missing arg5 (P_cutoff)}
+readonly max_iter=${6?Error: Missing arg6 (max_iter)}
+readonly min_mac=${7?Error: Missing arg7 (min_mac)}
 
-readonly step2_SPAtests="/well/lindgren/flassen/software/dev/SAIGE/extdata/step2_SPAtests.R"
+readonly threads=$(( ${NSLOTS}-1 ))
+readonly step2_SPAtests="utils/saige/step2_SPAtests.R"
 readonly rscript="scripts/conditional/03_spa_conditional.R"
 
 spa_chr_loop() {
+   local cond_vcf=${1}
+   local cond_markers=${2}
+   local cond_csi="${vcf}.csi"
    for chr in {1..22}; do
+      SECONDS=0
       local prefix_chr="${3}_chr${chr}"
-      set -x
       Rscript "${step2_SPAtests}"  \
-        --vcfFile=${1} \
-        --vcfFileIndex="${1}.csi" \
-        --vcfField="GT" \
-        --chrom="chr${chr}" \
-        --minMAF=0.000001 \
-        --minMAC=1 \
-        --GMMATmodelFile=${in_gmat} \
-        --varianceRatioFile=${in_var} \
-        --SAIGEOutputFile=${prefix_chr} \
-        --numLinesOutput=2 \
-        --IsOutputAFinCaseCtrl=TRUE \
-        --IsOutputNinCaseCtrl=TRUE \
-        --IsOutputHetHomCountsinCaseCtrl=TRUE \
-        --LOCO=FALSE \
-        ${2:+--condition "$2"}
-      set +x
+         --vcfFile=${cond_vcf} \
+         --vcfFileIndex=${cond_csi} \
+         --vcfField="GT" \
+         --chrom="chr${chr}" \
+         --minMAF=0.0000001 \
+         --minMAC=${min_mac} \
+         --GMMATmodelFile=${in_gmat} \
+         --varianceRatioFile=${in_var} \
+         --SAIGEOutputFile=${prefix_chr} \
+         --LOCO=FALSE \
+         ${cond_markers:+--condition "$cond_markers"} \
+         && print_update "Finished saddle-point approximation for chr${chr}" ${SECONDS} \
+         || raise_error "Saddle-point approximation for chr${chr} failed" 
    done
    >&2 echo "Markers: ${2}"
    >&2 echo "VCF: ${1}"
@@ -54,11 +54,12 @@ spa_chr_loop() {
 }
 
 conditional_analysis() {
-  markers_conditional="${out_prefix}_conditioning_markers.txt"
-  rm ${markers_conditional}
+  local markers_conditional="${out_prefix}_conditioning_markers.txt"
+  rm -f ${markers_conditional}
 
-  i=0 #
-  marker_list="" # markers for SAIGE
+  local i=0 
+  local marker_list=""
+  local current_marker=""
   while [ $i -lt $max_iter ]; do
 
       true $(( i++ ))
@@ -67,9 +68,9 @@ conditional_analysis() {
       >&2 echo "[Iteration ${i}]: Current condtioning marker list: ${marker_list}"
 
       # setup prefixes for temporary files
-      prefix_iter="${out_prefix}_i${i}"
-      prefix_iter_chr="${prefix_iter}_chr"
-      markers_combined="${prefix_iter}.txt"
+      local prefix_iter="${out_prefix}_i${i}"
+      local prefix_iter_chr="${prefix_iter}_chr"
+      local markers_combined="${prefix_iter}.txt"
 
       # Run saddle point approximation using condtioning markers
       spa_chr_loop "${vcf}" "${marker_list}" "${prefix_iter}"
@@ -82,15 +83,15 @@ conditional_analysis() {
         head -n1 >> "${markers_conditional}"
 
       # select current marker
-      old_marker=${current_marker}
-      current_marker=$( tail -n1 ${markers_conditional} | awk '{print $1":"$2"_"$4"/"$5}')
-      current_p=$( tail -n1 ${markers_conditional} | cut -d" " -f22)
+      local old_marker=${current_marker}
+      local current_marker=$( tail -n1 ${markers_conditional} | awk '{print $1":"$2"_"$4"/"$5}')
+      local current_p=$( tail -n1 ${markers_conditional} | cut -d" " -f22)
 
       if [[ "${current_marker}" != "${old_marker}" ]]; then
         if [[ "${marker_list}" == "" ]]; then
-          marker_list="${current_marker}"
+          local marker_list="${current_marker}"
         else
-          marker_list="${marker_list},${current_marker}"
+          local marker_list="${marker_list},${current_marker}"
         fi
         >&2 echo "[Iteration ${i}]: New marker found '${current_marker} (P-value=${current_p}). Conditioning on ${marker_list}'"
       else
@@ -102,6 +103,8 @@ conditional_analysis() {
 }
 
 # Run analysis
+set +eu
 set_up_RSAIGE
+set -eu
 conditional_analysis ${vcf} ${out_prefix}
 
