539bc97521aa889c39f4554cc982b1f60d49625a
frhl <frederik.heymann@gmail.com>
2022-03-28 21:34:12 +0100

    removed aggregation method from script, since we are now permuting phase seperately

--- scripts/05_knockouts.py
+++ scripts/05_knockouts.py
@@ -23,9 +23,7 @@ def main(args):
     
     chrom = int(args.chrom)
     only_vcf = args.only_vcf
-    randomize_phase = args.randomize_phase
     seed = args.seed
-    aggr_method = args.aggr_method
     checkpoint = args.checkpoint
 
     # run parser
@@ -33,20 +31,10 @@ def main(args):
     hl._set_flags(no_whole_stage_codegen='1') 
     mt = io.import_table(input_path, input_type)
   
-    if randomize_phase:
-        mt = mt.transmute_entries(GT=ko.rand_flip_call(mt.GT, seed=int(seed)))
-
-    # perform an aggregation based on "collect", which requires a lot
-    # of memory but allows the variant ID to be returned as well?
+    # perform an aggregation based on "collect", which requires a lot of memory
     gene_expr = mt.consequence.vep.worst_csq_by_gene_canonical.gene_id
-    if aggr_method in "fast":
-        print("[Note]: Using fast aggregation..")
-        genes = ko.aggr_phase_count_by_expr(mt, gene_expr)
-    elif aggr_method in "collect":
-        genes = ko.collect_phase_count_by_expr(mt, gene_expr) 
-        genes = ko.sum_gts_entries(genes)
-    else:
-        raise TypeError(str(aggr_method) + " is not allowed!") 
+    genes = ko.collect_phase_count_by_expr(mt, gene_expr) 
+    genes = ko.sum_gts_entries(genes)
 
     # calculate probability of being knocked out based on phased counts
     expr_pko = ko.calc_prob_ko(genes.hom_alt_n, genes.phased, genes.unphased)
@@ -69,7 +57,7 @@ def main(args):
     prob = prob.key_rows_by(prob.locus, prob.alleles)
     prob = prob.drop('gene_id')
     
-    # write out 
+    # write out variants involved and vcf
     io.export_table(prob, out_prefix, out_type)
     if not only_vcf:
         genes.filter_entries(genes.pKO > 0).entries().flatten().export(out_prefix + ".tsv.gz") 
@@ -82,10 +70,8 @@ if __name__=='__main__':
     parser.add_argument('--input_type', default=None, help='Input type, either "mt", "vcf" or "plink"')
     parser.add_argument('--out_prefix', default=None, help='Path prefix for output dataset')
     parser.add_argument('--out_type', default=None, help='Type of output dataset (options: mt, vcf, plink)')
-    parser.add_argument('--aggr_method', default="collect", help='How should the CH matrix be generated?')
     parser.add_argument('--only_vcf', default=False, action='store_true', help='Only return VCF (less memory required when running)')
     parser.add_argument('--checkpoint', default=False, action='store_true', help='Checkpoint gene-aggregation matrix to avoid Spark Memory overflow errors') 
-    parser.add_argument('--randomize_phase', default=None, action='store_true', help='Randomize phased calls?')
     parser.add_argument('--seed', default=None, help='Seed used for randomizing')
     
     args = parser.parse_args()
