ec2f3b2daced60c39175fdc27ee05aef639b529b
frhl <frederik.heymann@gmail.com>
2022-04-01 14:11:37 +0100

    now creating VCF of common conditioning SNPs for each chromosome and then later merging with pseudo variants with BCFtools

---
+++ scripts/conditional/04_extract_markers.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import hail as hl
+import argparse
+
+from ukb_utils import hail_init
+from ukb_utils import tables
+from ukb_utils import genotypes
+from ukb_utils import samples
+from ukb_utils import variants
+from ko_utils import io
+
+def main(args):
+    
+    chrom = args.chrom
+    ko_path = args.ko_path
+    ko_type = args.ko_type
+    markers = args.markers
+    out_prefix = args.out_prefix
+    out_type = args.out_type
+    final_sample_list = args.final_sample_list
+    
+    hail_init.hail_bmrc_init('logs/hail/combine_markers.log', 'GRCh38')
+    hl._set_flags(no_whole_stage_codegen='1')
+    markers = markers.split(",") # format chr12:4214:A:T
+    mt = genotypes.get_ukb_imputed_v3_bgen(chrom)
+    mt = samples.remove_withdrawn(mt)
+    mt = mt.annotate_rows(
+            marker = hl.delimit([
+                hl.str(mt.locus.contig), 
+                hl.str(mt.locus.position),
+                hl.str(mt.alleles[0]),
+                hl.str(mt.alleles[1])
+                ],':'))
+    mt = mt.filter_rows(hl.literal(set(markers)).contains(mt.marker))
+    mt = variants.liftover(mt)
+    mt = mt.annotate_entries(DS=hl.float64(mt.GT.n_alt_alleles()))
+    mt = mt.select_entries(mt.DS)
+    
+    if final_sample_list:
+        ht_final_samples = hl.import_table(
+                final_sample_list,
+                no_header=True, key='f0',
+                delimiter=',')
+        mt = mt.filter_cols(hl.is_defined(ht_final_samples[mt.col_key])) 
+    
+    # export variants to be used for conditional analysis
+    io.export_table(mt, out_prefix, out_type)
+
+if __name__=='__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--chrom', default=None, help='')
+    parser.add_argument('--ko_path', default=None, help='')
+    parser.add_argument('--ko_type', default=None, help='')
+    parser.add_argument('--markers', default=None, help='')
+    parser.add_argument('--out_prefix', default=None, help='')
+    parser.add_argument('--out_type', default=None, help='')
+    parser.add_argument('--final_sample_list', default=None, help='')
+    args = parser.parse_args()
+
+    main(args)
+
+
+
