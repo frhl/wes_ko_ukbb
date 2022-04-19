14741a53dccd251719317bfbd04877a5b737783c
frhl <frederik.heymann@gmail.com>
2022-04-19 11:21:59 +0100

    changed qsub logs

--- scripts/prs/_prs_clean.sh
+++ scripts/prs/_prs_clean.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
-#$ -N _rm_bfile
+#$ -N _prs_clean
 #$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
-#$ -o logs/_rm_bfile.log
-#$ -e logs/_rm_bfile.errors.log
+#$ -o logs/_prs_clean.log
+#$ -e logs/_prs_clean.errors.log
 #$ -V
 
 readonly pred=${1?Error: Missing arg3 (prediction file)}
