c67c8dc1067f4f803624342a9f5ab670cd0697b9
frhl <frederik.heymann@gmail.com>
2022-05-12 13:21:46 +0100

    now checkinf if file exists before starting R

--- scripts/conditional/common/01_gene_positions.sh
+++ scripts/conditional/common/01_gene_positions.sh
@@ -9,23 +9,26 @@
 #$ -P lindgren.prjc
 #$ -pe shmem 1
 #$ -q short.qf
-#$ -t 1-80
+#$ -t 1-44
 #$ -tc 10
 
-#set -o errexit
-#set -o nounset
+set -o errexit
+set -o nounset
 
 source utils/qsub_utils.sh
 source utils/bash_utils.sh
 
 readonly rscript="scripts/conditional/common/01_gene_positions.R"
 
+readonly maf="0to5e-2"
+readonly min_mac=4
+
 readonly genes="data/genes/220310_ensgid_grch38_pos.tsv.gz"
-readonly out_dir="data/conditional/common/intervals"
+readonly out_dir="data/conditional/common/gene_positions/min_mac${min_mac}"
 readonly pheno_dir="data/phenotypes"
 readonly in_prefix="ukb_eur_wes_200k"
-readonly maf="0to5e-2"
 
+mkdir -p ${out_dir}
 
 submit_binary_intervals()
 {
@@ -49,21 +52,21 @@ submit_intervals()
   local annotation=${1?Error: Missing arg1 (consequence)}
   local phenotype=${2?Error: Missing arg2 (phenotype)}
   local trait=${3?Error: Missing arg3 (trait)}
-  local step2_dir="data/saige/output/${trait}/step2"
-  
+  local step2_dir="data/saige/output/${trait}/step2/min_mac${min_mac}"
   local in_file="${step2_dir}/${in_prefix}_maf${maf}_${phenotype}_${annotation}.txt.gz"
   local out_prefix="${out_dir}/${in_prefix}_maf${maf}_${phenotype}_${annotation}"
-  
-  mkdir -p ${out_dir}
-  
-  set -x
-  Rscript "${rscript}" \
-    --in_spa_file "${in_file}" \
-    --coordinates "${genes}" \
-    --flanking_bp 0 \
-    --fdr_cutoff "0.25" \
-    --out_prefix "${out_prefix}"
-  set +x
+  if [ -f ${in_file} ]; then
+    set -x
+    Rscript "${rscript}" \
+      --in_spa_file "${in_file}" \
+      --coordinates "${genes}" \
+      --flanking_bp 0 \
+      --fdr_cutoff "0.25" \
+      --out_prefix "${out_prefix}"
+    set +x
+  else
+    >&2 echo "${in_file} does not exist. Skipping!"
+  fi
 }
 
 submit_binary_intervals "pLoF_damaging_missense"
