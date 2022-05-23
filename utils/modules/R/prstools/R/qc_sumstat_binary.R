acb82c67493d764fea79f421efed562cd58f353a
frhl <frederik.heymann@gmail.com>
2022-05-23 09:56:52 +0100

    updated documentation

--- utils/modules/R/prstools/R/qc_sumstat_binary.R
+++ utils/modules/R/prstools/R/qc_sumstat_binary.R
@@ -1,6 +1,9 @@
 #' @title quality control of binary summary statistics
-#' @param G genotypes from bigsnpr
-#' @param info_snp info from bigsnpr
+#' @param G genotypes object from bigsnpr
+#' @param info_snp info object from bigsnpr (see
+#' @param ncores cores to be used for parallel.
+#' @references code from \url{https://privefl.github.io/bigsnpr-extdoc/polygenic-scores-pgs.html}
+#' @return a list of vectors and numerics.
 #' @export
 
 qc_sumstat_binary <- function(G, info_snp, ncores = 1) {
@@ -14,7 +17,6 @@ qc_sumstat_binary <- function(G, info_snp, ncores = 1) {
     is_bad <- (is_bad_sd | is_bad_maf)
     sum_is_bad <- sum(is_bad, na.rm = TRUE)
     sum_n <- length(is_bad)
-    write(paste(sum_is_bad, 'of', sum_n, 'SNPs fail QC'), stderr())
     return(list(is_bad = is_bad, sd_val = sd_val, sd_ss = sd_ss))
 }
 
