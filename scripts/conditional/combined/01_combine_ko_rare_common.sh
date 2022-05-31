cb0c1e482f272b75c7aa06d550e576a81b699af1
frhl <frederik.heymann@gmail.com>
2022-05-31 16:05:35 +0100

    added scripts for combining rare and common variants

---
+++ scripts/conditional/combined/01_combine_ko_rare_common.sh
@@ -0,0 +1,72 @@
+#!/usr/bin/env bash
+#
+# Append pseudo variants with actual variants for downstream conditional analysis.
+#
+#$ -N append_vcf_rare
+#$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
+#$ -o logs/append_vcf_rare.log
+#$ -e logs/append_vcf_rare.errors.log
+#$ -P lindgren.prjc
+#$ -q short.qc
+#$ -pe shmem 3
+#$ -t 1-22
+#$ -V
+
+
+set -o errexit
+set -o nounset
+
+source utils/vcf_utils.sh
+source utils/qsub_utils.sh
+source utils/bash_utils.sh
+source utils/hail_utils.sh
+
+readonly spark_dir="data/tmp/spark"
+readonly hail_script="scripts/conditional/rare/01_append_vcf_rare.py"
+
+readonly chr="${SGE_TASK_ID}"
+readonly variants_dir="data/mt/annotated"
+
+# note: assuming ko and rare variants have already been merged
+readonly ko_rare_dir="data/conditional/rare/combined"
+readonly common_dir="data/conditional/common/combined"
+readonly out_dir="data/conditional/combined"
+
+readonly ko_rare_path="${ko_rare_dir}/ukb_eur_wes_200k_chr${chr}_maf0to5e-2_pLoF_damaging_missense.mt"
+readonly common_path="${common_dir}/ukb_eur_wes_200k_chr${chr}_maf0to5e-2_pLoF_damaging_missense.mt"
+readonly out_prefix="${out_dir}/ukb_eur_wes_200k_chr${chr}_maf0to5e-2_pLoF_damaging_missense"
+
+readonly ko_rare_type="mt"
+readonly common_type="mt"
+readonly out_type="vcf"
+
+readonly maf_max=0.05
+readonly maf_min=0.00
+readonly in_sex="both"
+readonly category="pLoF,damaging_missense"
+readonly exclude="data/genes/220310_common_plofs_to_exclude.txt"
+
+mkdir -p ${out_dir}
+
+
+if [ ! -f "${out_prefix}.vcf.bgz" ]; then
+  SECONDS=0
+  set_up_hail
+  set_up_pythonpath_legacy
+  python3 "${hail_script}" \
+     --ko_rare_path ${ko_rare_path} \
+     --ko_rare_type ${ko_rare_type} \
+     --common_path ${common_path} \
+     --common_type ${common_type} \
+     --out_prefix ${out_prefix} \
+     --out_type ${out_type} \
+     && print_update "Finished merging knockouts with markers ${out_prefix}" ${SECONDS} \
+     || raise_error "Merging knockouts with markers for ${out_prefix} failed!"
+fi
+
+if [ ! -f "${out_prefix}.vcf.csi" ]; then
+  module purge
+  module load BCFtools/1.12-GCC-10.3.0
+  make_tabix "${out_prefix}.vcf.bgz" "csi"
+else
+
