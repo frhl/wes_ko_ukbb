4ac119646d8a81b665988bd91f460b272fd5dd4c
frhl <frederik.heymann@gmail.com>
2022-06-28 11:13:30 +0100

    now GWAS possible again

--- scripts/prs/03_gwas.sh
+++ scripts/prs/03_gwas.sh
@@ -7,7 +7,7 @@
 #$ -P lindgren.prjc
 #$ -pe shmem 1
 #$ -q test.qc
-#$ -t 1-2
+#$ -t 1
 #$ -tc 1
 #$ -V
 
@@ -20,8 +20,7 @@ source utils/bash_utils.sh
 source utils/hail_utils.sh
 
 readonly in_dir="data/prs/hapmap/ukb_500k/fitting"
-readonly pheno_header_dir="data/phenotypes"
-readonly pheno_dir="/well/lindgren/UKBIOBANK/dpalmer/ukb_wes_phenotypes"
+readonly pheno_dir="data/phenotypes"
 
 readonly bash_script="scripts/prs/_gwas.sh"
 readonly hail_script="scripts/prs/03_gwas.py"
@@ -35,12 +34,12 @@ readonly input_path="${in_dir}/ukb_hapmap_500k_eur_chrCHR"
 
 readonly index=${SGE_TASK_ID}
 
-readonly file_cts="${pheno_dir}/curated_phenotypes_cts.tsv" 
-readonly pheno_list_cts="${pheno_header_dir}/filtered_phenotypes_cts_manual.tsv"
+readonly file_cts="${pheno_dir}/curated_covar_phenotypes_cts.tsv.gz" 
+readonly pheno_list_cts="${pheno_dir}/filtered_phenotypes_cts_manual.tsv"
 readonly phenotype_cts=$( sed "${index}q;d" ${pheno_list_cts} )
 
-readonly file_binary="${pheno_dir}/curated_phenotypes_binary.tsv"
-readonly pheno_list_binary="${pheno_header_dir}/filtered_phenotypes_binary_header.tsv"
+readonly file_binary="${pheno_dir}/curated_covar_phenotypes_binary.tsv.gz"
+readonly pheno_list_binary="${pheno_dir}/filtered_phenotypes_binary_header.tsv"
 readonly phenotype_binary=$( sed "${index}q;d" ${pheno_list_binary} )
 
 readonly min_cases=100
