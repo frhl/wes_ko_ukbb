de60e517c3e5522874d61d13ab093b1d1014e512
frhl <frederik.heymann@gmail.com>
2022-07-05 17:31:23 +0100

    now able to run script for simple evaluation of chunks

--- scripts/phasing/07_eval_chunks.sh
+++ scripts/phasing/07_eval_chunks.sh
@@ -7,43 +7,22 @@
 #$ -P lindgren.prjc
 #$ -pe shmem 1
 #$ -q short.qe
-#$ -t 20
 #$ -V
 
 
 source utils/bash_utils.sh
 
-readonly rscript="scripts/phasing/06_eval_chunks.R"
-
-#readonly chr=$( get_chr ${SGE_TASK_ID} )
-readonly main_dir="data/phased/wes_union_calls/chunks/final"
-
-END=22
-in_dir=""
-for chr in $(seq 1 $END); do 
-  dir="${main_dir}/ukb_eur_wes_union_calls_200k_chr${chr}-16xshort.qe"
-  in_dir="${in_dir},${dir}"
-done
-
-#readonly dir1="${main_dir}/ukb_wes_union_calls_200k_chr20-16xshort.qa"
-#readonly dir2="${main_dir}/ukb_wes_union_calls_200k_chr20-24xshort.qa"
-#readonly dir3="${main_dir}/ukb_wes_union_calls_200k_chr20-16xlong.qc"
-#readonly in_dir="${dir1},${dir2},${dir3}"
-
-readonly in_dir_merged="data/phased/wes_union_calls/merged"
-
-readonly out_dir="derived/phased/wes_union_calls/qc"
+readonly rscript="scripts/phasing/07_eval_chunks.R"
+readonly main_dir="data/phased/wes_union_calls/chunks"
+readonly out_dir="derived/phased/wes_union_calls/validation"
 readonly out_prefix="${out_dir}/ukb_eur_wes_union_calls"
 
 mkdir -p ${out_dir}
 
-
 set_up_rpy
-set -x
 Rscript ${rscript} \
-    --in_dir "${in_dir}" \
-    --in_dir_merged "${in_dir_merged}" \
+    --master_chunk_dir "${main_dir}" \
     --out_prefix "${out_prefix}" \
-    --img_height 12
-set +x
+    --img_height 12 \
+    --img_width 8
 
