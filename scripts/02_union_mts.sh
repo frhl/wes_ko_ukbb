c00483f580d0d3a30b03eb9e56c9a6139c30e95d
frhl <frederik.heymann@gmail.com>
2022-07-07 11:15:08 +0100

    checking if input phased file is present

--- scripts/02_union_mts.sh
+++ scripts/02_union_mts.sh
@@ -5,9 +5,9 @@
 #$ -o logs/union_mts.log
 #$ -e logs/union_mts.errors.log
 #$ -P lindgren.prjc
-#$ -pe shmem 5
+#$ -pe shmem 6
 #$ -q long.qc@@long.hga
-#$ -t 14-15
+#$ -t 1-22
 
 set -o errexit
 set -o nounset
@@ -19,14 +19,14 @@ readonly spark_dir="data/tmp/spark_dir"
 
 # input files
 readonly chr=$( get_chr ${SGE_TASK_ID} ) 
-readonly in_dir_phased="data/phased/wes_union_calls/ligated/new"
+readonly in_dir_phased="data/phased/wes_union_calls/ligated"
 readonly in_phased="${in_dir_phased}/ukb_eur_wes_union_calls_200k_chr${chr}.vcf.bgz"
 readonly in_dir_unphased="data/unphased/wes/post-qc"
 readonly in_unphased="${in_dir_unphased}/ukb_wes_200k_filtered_chr${chr}.mt"
 
 # output files
-readonly out_dir="data/mt/union/14_15"
-readonly out_prefix="${out_dir}/ukb_eur_wes_200k_union_chr${chr}"
+readonly out_dir="data/mt/union"
+readonly out_prefix="${out_dir}/ukb_eur_wes_union_calls_200k_chr${chr}"
 readonly out="${out_prefix}.mt"
 
 # hail script
@@ -34,20 +34,24 @@ readonly hail_script="scripts/02_union_mts.py"
 
 mkdir -p ${out_dir}
 if [ ! -f "${out}/_SUCCESS" ]; then
-  SECONDS=0
-  set_up_hail
-  set_up_pythonpath_legacy  
-  python3 "${hail_script}" \
-     --chrom ${chr} \
-     --input_phased ${in_phased}\
-     --input_unphased ${in_unphased} \
-     --input_phased_type "vcf" \
-     --input_unphased_type "mt" \
-     --out_prefix ${out_prefix} \
-     --out_type "mt" \
-     && print_update "Finished merging phased and unphased data for chr${chr}" ${SECONDS} \
-     || raise_error "Merging phased and unphased data for chr${chr} failed"
-  log_runtime ${SECONDS}
+  if [ -f "${in_phased}" ]; then
+    SECONDS=0
+    set_up_hail
+    set_up_pythonpath_legacy  
+    python3 "${hail_script}" \
+       --chrom ${chr} \
+       --input_phased ${in_phased}\
+       --input_unphased ${in_unphased} \
+       --input_phased_type "vcf" \
+       --input_unphased_type "mt" \
+       --out_prefix ${out_prefix} \
+       --out_type "mt" \
+       && print_update "Finished merging phased and unphased data for chr${chr}" ${SECONDS} \
+       || raise_error "Merging phased and unphased data for chr${chr} failed"
+    log_runtime ${SECONDS}
+  else
+    >&2 echo "${in_phased} (input) does not exists. Exiting.."
+  fi
 else
   print_update "file ${out} already exists. Skipping!"
 fi
