8bb8a4a1ac8b713bc083b69e0bd07b139856aa63
frhl <frederik.heymann@gmail.com>
2022-07-20 14:45:18 +0100

    can now merge all knockout files

--- scripts/geneset/03_export_ko_geneset.sh
+++ scripts/geneset/03_export_ko_geneset.sh
@@ -16,16 +16,13 @@ source utils/bash_utils.sh
 source utils/qsub_utils.sh
 source utils/hail_utils.sh
 
-readonly spark_dir="data/tmp/spark_dir"
-readonly hail_script="scripts/04_export_csqs.py"
-readonly rscript="scripts/04_export_csqs_test.R"
+readonly rscript="scripts/geneset/02_export_ko_geneset.R"
 
 readonly chr=$( get_chr ${SGE_TASK_ID} ) 
-readonly in_dir="data/mt/annotated"
-readonly input_prefix="${in_dir}/ukb_eur_wes_200k_annot_chr${chr}.mt"
+readonly in_dir="data/knockout/alt"
+readonly in_vcf="${in_dir}/ukb_eur_wes_200k_chr${chr}_maf0to5e-2_pLoF_damaging_missense.vcf.bgz"
 
 readonly out_dir="data/mt/vep"
-#readonly out_prefix="${out_dir}/ukb_eur_wes_200k_csqs_chr${chr}"
 readonly out_prefix="${out_dir}/ukb_eur_wes_200k_csqs_chrALL_test"
 readonly out_saige="${out_prefix}.saige"
 
