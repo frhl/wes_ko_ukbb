f890fab5bbff96b3bb1dd13951dbbab8c5e141c5
frhl <frederik.heymann@gmail.com>
2022-07-20 10:54:40 +0100

    now selecting PRS if available otherwise standard file and subsetting by exome-wide significance

--- scripts/conditional/common/01_gene_positions.R
+++ scripts/conditional/common/01_gene_positions.R
@@ -4,29 +4,35 @@ library(data.table)
 
 main <- function(args){
 
+    stopifnot(file.exists(args$in_spa_file))
+    stopifnot(file.exists(args$coordinates))
+
     spa <- fread(args$in_spa_file)
+    spa_n <- nrow(spa)
     positions <- fread(args$coordinates)
     mrg <- merge(spa, positions, all.x = TRUE, by.x = 'MarkerID', by.y = 'ensembl_gene_id')
-        
-    mrg$fdr <- stats::p.adjust(mrg$p.value, method = 'fdr')
-    mrg <- mrg[order(mrg$fdr),]
-    mrg <- mrg[mrg$fdr < args$fdr_cutoff,]
 
     out <- data.table(
         gene = gsub("\\..+$",'',mrg$ensembl_gene_id),
-        fdr = mrg$fdr,
+        pvalue = mrg$p.value,
         contig = mrg$chromosome_name,
         start = mrg$start_position - as.numeric(args$flanking_bp),
         end = mrg$end_position + as.numeric(args$flanking_bp)
      )
 
-    if (nrow(out) > 0){
+    # filter to signifcant entries
+    p_cutoff <- as.numeric(args$p_cutoff)
+    out <- out[out$pvalue < p_cutoff,]
+    n <- nrow(out)
+    msg <- paste("Subsetted", args$phenotype, "to", n, "significant entries in", args$in_spa_file)
+    write(msg, stdout())
+
+
+    if (n > 0){
         outfile = paste0(args$out_prefix, '.tsv.gz')
         write(paste("writing to", outfile), stderr())
         fwrite(out, outfile, sep = '\t')
-    } else {
-        write(paste("No genes signifcant at FDR <",args$fdr_cutoff),stderr())
-    }
+    } 
 }
 
 parser <- ArgumentParser()
@@ -35,6 +41,8 @@ parser$add_argument("--in_spa_file", default=NULL, help = "in directory")
 parser$add_argument("--coordinates", default=NULL, help = "prefix of files to be loaded")
 parser$add_argument("--flanking_bp", default=0, help = "prefix of files to be loaded")
 parser$add_argument("--fdr_cutoff", default=0.20, help = "Path to list of phenotypes to be loaded.")
+parser$add_argument("--p_cutoff", default=5e-6, help = "Path to list of phenotypes to be loaded.")
+parser$add_argument("--phenotype", default=NULL, help = "Current phenotype")
 args <- parser$parse_args()
 
 main(args)
