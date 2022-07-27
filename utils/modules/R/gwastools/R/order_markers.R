f935766cf8159e4184020b461006d64766b9a8e5
frhl <frederik.heymann@gmail.com>
2022-07-27 22:20:23 +0100

    adeded new function for ordering marker IDs

---
+++ utils/modules/R/gwastools/R/order_markers.R
@@ -0,0 +1,32 @@
+#' @title Order markers by position
+#' @param x vector of markers to be ordered
+#' @return a vector of oredered markers.
+
+order_markers <- function(x, rm.dup = TRUE) {
+    
+    # check integrity
+    regex <- "chr[0-9]+\\:[0-9]+\\:[a-zA-Z]+\\:[a-zA-Z]"
+    stopifnot(grepl(regex, x))
+    
+    # format/order by position
+    d <- data.frame(do.call(rbind, strsplit(x, split = ':')))
+    colnames(d) <- c("chr","pos","a1", "a2")
+    d$pos <- as.numeric(d$pos)
+    d$chr_int <- as.numeric(gsub("chr","",d$chr))
+    d <- d[order(d$chr_int, d$pos),]
+    d$chr_int <- NULL
+    
+    # deal with duplicates
+    if (rm.dup){
+      dups <- duplicated(d)
+      d <- d[!dups,]
+      if (sum(dups) > 0) warning("Some markers are duplicated. These have been discarded.")
+    }
+ 
+    # format out vector
+    out <- apply( d , 1 , paste , collapse = ":")
+    out <- as.character(gsub(" ", "", out))
+    return(out)
+}
+
+
