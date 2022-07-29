3b253fc85ee756e77e009a0cf48d9bfffb3ebfee
frhl <frederik.heymann@gmail.com>
2022-07-29 10:07:34 +0100

    added better warnings/stops

--- utils/modules/R/gwastools/R/order_markers.R
+++ utils/modules/R/gwastools/R/order_markers.R
@@ -7,8 +7,13 @@ order_markers <- function(x, rm.dup = TRUE) {
     
     # check integrity
     regex <- "chr[0-9]+\\:[0-9]+\\:[a-zA-Z]+\\:[a-zA-Z]"
-    stopifnot(grepl(regex, x))
-    
+    ok <- grepl(regex, x)
+    if (any(!ok)){
+      example <- x[!ok][1]
+      n <- sum(!ok)
+      stop(paste0(n, " marker(s) were not formatted correctly, e.g. '", example,"'."))
+    }
+
     # format/order by position
     d <- data.frame(do.call(rbind, strsplit(x, split = ':')))
     colnames(d) <- c("chr","pos","a1", "a2")
