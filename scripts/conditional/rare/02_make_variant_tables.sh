6b7a0cee560beba4431bdaf314b404f136b9c4ef
frhl <frederik.heymann@gmail.com>
2022-08-01 11:56:44 +0100

    moved back to parallel workflow in which chunks of rows of variants are parallelized on the BMRC

---
+++ scripts/conditional/rare/02_make_variant_tables.sh
@@ -0,0 +1,99 @@
+#!/usr/bin/env bash
+#
+#$ -N make_variant_tables
+#$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
+#$ -o logs/make_variant_tables.log
+#$ -e logs/make_variant_tables.errors.log
+#$ -P lindgren.prjc
+#$ -pe shmem 1
+#$ -q short.qa
+#$ -t 19-22
+#$ -V
+
+set -o errexit
+set -o nounset
+
+module purge
+source utils/bash_utils.sh
+
+readonly make_script="scripts/conditional/rare/_make_variant_tables.sh"
+readonly merge_script="scripts/conditional/rare/_merge_variant_tables.sh"
+
+readonly chr="${SGE_TASK_ID}"
+readonly pheno_dir="data/phenotypes"
+readonly in_dir="data/conditional/rare/combined"
+readonly out_dir="data/conditional/rare/combined/chunks_parallel"
+
+readonly in_vcf="${in_dir}/ukb_eur_wes_200k_chr${chr}_maf0to5e-2_pLoF_damaging_missense.vcf.bgz"
+readonly covar_path="${pheno_dir}/covars1.csv"
+
+readonly out_prefix="${out_dir}/ukb_eur_wes_200k_chr${chr}_maf0to5e-2_pLoF_damaging_missense"
+readonly out_mrg="${out_dir}/ukb_eur_wes_200k_chr${chr}_maf0to5e-2_pLoF_damaging_missense"
+
+#readonly pheno_cts_path="{pheno_dir}/curated_covar_phenotypes_cts_200k.tsv"
+#readonly phenotypes_cts="{pheno_dir}/filtered_phenotypes_cts_manual.tsv"
+
+mkdir -p ${out_dir}
+
+
+int_div () {
+  echo $(( $(( ${1} + ${2} - 1)) / ${2} ))
+}
+
+# how many chunks should be employed
+readonly vcf_lines=$( zcat "${in_vcf}" | grep -v "#" | cut -f1 | wc -l )
+readonly lines_per_chunk=1000
+readonly chunks=$( int_div ${vcf_lines} ${lines_per_chunk})
+
+# hpc paramaters
+readonly queue="short.qc"
+readonly nslots=1
+readonly tasks="1-${chunks}"
+
+submit_binary(){
+  local trait="binary"
+  local pheno_list="${pheno_dir}/filtered_phenotypes_binary_header.tsv"
+  local pheno_file="${pheno_dir}/curated_covar_phenotypes_binary_200k.tsv" 
+  submit_qc_job ${pheno_list} ${pheno_file} ${trait}
+}
+
+submit_qc_job() {
+  local pheno_list=${1}
+  local pheno_file=${2}
+  local trait=${3}
+  local qsub_main="_pref_c${chr}_${trait}"
+  local qsub_mrg="_mrg_c${chr}"
+  local pheno_list_csv=$(cat ${pheno_list} | tr "\n" ",")
+
+
+  # submit main script
+  qsub -N "${qsub_main}" \
+      -t ${tasks} \
+      -q "${queue}" \
+      -pe shmem ${nslots} \
+      "${make_script}" \
+      "${in_vcf}" \
+      "${vcf_lines}" \
+      "${lines_per_chunk}" \
+      "${chunks}" \
+      "${pheno_file}" \
+      "${pheno_list_csv}" \
+      "${covar_path}" \
+      "${out_prefix}"
+
+  # submit merge job
+  qsub -N "${qsub_mrg}" \
+    -q short.qc@@short.hge \
+    -pe shmem 1 \
+    -hold_jid "${qsub_main}" \
+    "${merge_script}" \
+    "${out_prefix}" \
+    "${chunks}" \
+    "${out_mrg}"
+
+}
+
+submit_binary
+
+
+
---
+++ scripts/conditional/rare/_make_variant_tables.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+#
+#$ -N _make_variant_tables
+#$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
+#$ -o logs/_make_variant_tables.log
+#$ -e logs/_make_variant_tables.errors.log
+#$ -P lindgren.prjc
+#$ -q short.qa
+#$ -pe shmem 1
+#$ -V
+
+set -o errexit
+set -o nounset
+
+source utils/bash_utils.sh
+
+readonly rscript="scripts/conditional/rare/_make_variant_tables.R"
+
+readonly in_vcf=${1?Error: Missing arg1 (in_vcf)}
+readonly vcf_lines=${2?Error: Missing arg2 (vcf_lines)}
+readonly lines_per_chunk=${3?Error: Missing arg3 (lines_per_chunk)}
+readonly total_chunks=${4?Error: Missing arg4 (total_chunks)}
+readonly pheno_file=${5?Error: Missing arg5 (pheno_file)}
+readonly pheno_list_csv=${6?Error: Missing arg6 (pheno_list_csv)}
+readonly covar_path=${7?Error: Missing arg7 (covar_path)}
+readonly out_prefix=${8?Error: Missing arg8 (out_prefix)}
+
+readonly chunk=${SGE_TASK_ID}
+readonly out_prefix_chunk="${out_prefix}.${chunk}of${total_chunks}"
+
+set_up_rpy
+Rscript "${rscript}" \
+  --in_vcf "${in_vcf}" \
+  --vcf_lines "${vcf_lines}" \
+  --lines_per_chunk "${lines_per_chunk}" \
+  --chunk "${chunk}" \
+  --total_chunks "${total_chunks}" \
+  --pheno_file "${pheno_file}" \
+  --phenotypes "${pheno_list_csv}" \
+  --covariates "${covar_path}" \
+  --out_prefix "${out_prefix_chunk}"
+
+
+
