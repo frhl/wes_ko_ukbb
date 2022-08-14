f03efe780e5e184309830edee7ac8b968603ab0f
frhl <frederik.heymann@gmail.com>
2022-08-14 08:41:43 +0100

    now differentiating between variants exported by gene or by variants themselves

--- scripts/04_export_csqs_by_variants.sh
+++ scripts/04_export_csqs_by_variants.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #
-#$ -N export_csqs
+#$ -N export_csqs_by_variant
 #$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
-#$ -o logs/export_csqs.log
-#$ -e logs/export_csqs.errors.log
+#$ -o logs/export_csqs_by_variant.log
+#$ -e logs/export_csqs_by_variant.errors.log
 #$ -P lindgren.prjc
 #$ -pe shmem 1
 #$ -q short.qc@@short.hga
@@ -24,7 +24,7 @@ readonly chr=$( get_chr ${SGE_TASK_ID} )
 readonly in_dir="data/mt/annotated"
 readonly input_prefix="${in_dir}/ukb_eur_wes_union_calls_200k_chr${chr}.mt"
 
-readonly out_dir="data/mt/vep/worst_csq_by_gene_canonical"
+readonly out_dir="data/mt/vep/worst_csq_for_variant_canonical"
 readonly out_prefix="${out_dir}/ukb_eur_wes_union_calls_200k_chr${chr}"
 readonly out_saige="${out_prefix}.saige"
 
@@ -38,8 +38,7 @@ if [ ! -f "${out_prefix}.tsv.gz" ]; then
      --in_file ${input_prefix}\
      --in_type "mt" \
      --out_prefix ${out_prefix} \
-     --by "worst_csq_by_gene_canonical" \
-     --by_explode \
+     --by "worst_csq_for_variant_canonical" \
      --out_type "tsv" \
      && print_update "Finished exporting csqs chr${chr}" ${SECONDS} \
      || raise_error "Exporting csqs for chr${chr} failed"
