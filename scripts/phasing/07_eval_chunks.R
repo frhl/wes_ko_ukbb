fe55492c3ddc9dfb4927efd3a2648e99b24ec53b
frhl <frederik.heymann@gmail.com>
2022-08-24 10:34:00 +0100

    fixed minor variable missing error

--- scripts/phasing/07_eval_chunks.R
+++ scripts/phasing/07_eval_chunks.R
@@ -96,7 +96,7 @@ main <- function(args){
     w = as.numeric(args$img_width) 
     out_p1 <- paste0(args$out_prefix, "_chunks_by_ser.png")
     out_d1 <- paste0(args$out_prefix, "_chunks_by_ser.txt.gz")
-    write(paste0("writing ",h,"x", w," png to",out_p1,), stdout())
+    write(paste0("writing ",h,"x", w," png to",out_p1), stdout())
     ggsave(out_p1, p1, width = w, height = h)
     fwrite(counts, out_d1, sep = "\t") 
 
