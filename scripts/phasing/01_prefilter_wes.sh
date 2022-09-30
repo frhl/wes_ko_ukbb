13864f918b46ef7fadeb976d2feae5286ce1b01d
frhl <frederik.heymann@gmail.com>
2022-09-30 16:46:12 +0100

    checkpoint

--- scripts/phasing/01_prefilter_wes.sh
+++ scripts/phasing/01_prefilter_wes.sh
@@ -10,7 +10,7 @@
 #SBATCH --error=logs/prefilter_wes.errors.log
 #SBATCH --partition=short
 #SBATCH --cpus-per-task 2
-#SBATCH --array=20-22
+#SBATCH --array=1-22
 
 
 source utils/qsub_utils.sh
--- scripts/phasing/02_wes_union_calls_gen.sh
+++ scripts/phasing/02_wes_union_calls_gen.sh
@@ -9,7 +9,7 @@
 #SBATCH --error=logs/wes_union_calls_gen.errors.log
 #SBATCH --partition=short
 #SBATCH --cpus-per-task 2
-#SBATCH --array=20-22
+#SBATCH --array=1-22
 
 source utils/qsub_utils.sh
 source utils/hail_utils.sh
