2ee8d34a4d5dabbc02af38376aee774426140b4c
frhl <frederik.heymann@gmail.com>
2022-10-07 11:24:50 +0100

    FIX: if permuted P is not changing, return last P

--- scripts/permute/_calc_p.sh
+++ scripts/permute/_calc_p.sh
@@ -5,33 +5,26 @@ set -o nounset
 
 source utils/bash_utils.sh
 
-
 readonly r_calc_emp_p="scripts/permute/_calc_empirical_p.R"
 readonly r_get_spa_p="scripts/permute/_get_spa_p.R"
 readonly rlogic="scripts/permute/_logic.R"
 readonly r_get_true_p="scripts/permute/_get_true_p.R"
 
-readonly gene=${1?Error: Missing arg1 (prefix)}
-readonly phenotype=${2?Error: Missing arg1 (prefix)}
-readonly annotation=${3?Error: Missing arg1 (prefix)}
-readonly static_assoc=${4?Error: Missing arg1 (prefix)}
-readonly use_prs=${5?Error: Missing arg1 (prefix)}
-readonly prs_available=${6?Error: Missing arg1 (prefix)}
-readonly saige_merged=${7?Error: Missing arg1 (prefix)}
-readonly top_p=${8?Error: Missing arg1 (prefix)}
-readonly true_p_path=${9?Error: Missing arg1 (prefix)}
-readonly n_shuffle=${10?Error: Missing arg1 (prefix)}
-readonly out_prefix=${11?Error: Missing arg1 (prefix)}
+readonly gene=${1?Error: Missing arg1 (gene)}
+readonly phenotype=${2?Error: Missing arg2 (prefix)}
+readonly annotation=${3?Error: Missing arg3 (prefix)}
+readonly static_assoc=${4?Error: Missing arg4 (prefix)}
+readonly use_prs=${5?Error: Missing arg5 (prefix)}
+readonly prs_available=${6?Error: Missing arg6 (prefix)}
+readonly saige_merged=${7?Error: Missing arg7 (prefix)}
+readonly top_p=${8?Error: Missing arg8 (prefix)}
+readonly true_p_path=${9?Error: Missing arg9 (prefix)}
+readonly n_shuffle=${10?Error: Missing arg10 (prefix)}
+readonly iteration=${11?Error: Missing arg11 (prefix)}
+readonly out_prefix=${12?Error: Missing arg12 (prefix)}
 
 readonly outfile="${out_prefix}.permuted"
 readonly pfile="${out_prefix}_${phenotype}.pvalues"
-
-#echo "${gene}"
-#echo "${true_p_path}"
-#echo "${phenotype}"
-#echo "${use_prs}"
-#echo "${static_assoc}"
-#echo "${annotation}"
 
 
 lookup_true() {
@@ -60,10 +53,10 @@ get_last_permuted_p() {
 }
 
 set_up_rpy
-if [ -f ${saige_merged} ]; then
+if [ -f "${saige_merged}" ]; then
   readonly true_p=$( lookup_true "p" )
   readonly true_t=$( lookup_true "t" )
-  if [ $(zcat ${saige_merged} | wc -l) -gt 1 ]; then
+  if [ "$(zcat ${saige_merged} | wc -l)" -gt "1" ]; then
     if [[ "${true_p}" != "NA" ]]; then
       readonly last_p=$(get_last_permuted_p)
       readonly permuted_p=$(Rscript ${r_get_spa_p} --input_path "${saige_merged}" --select_min_p ${top_p} )
@@ -71,14 +64,14 @@ if [ -f ${saige_merged} ]; then
       readonly tmp_empirical_p=$(
           Rscript ${r_calc_emp_p} \
             --input_path "${saige_merged}" \
-            --true_tstat ${true_t} \
-            --true_p ${true_p} \
+            --true_tstat "${true_t}" \
+            --true_p "${true_p}" \
             --out_prefix "${pfile}.tmp")
       echo "[msg]: ${phenotype} ${gene}. Last P = ${last_p}"
       echo "[msg]: ${phenotype} ${gene}. Permuted P = ${permuted_p}"
       echo "[msg]: ${phenotype} ${gene}. Empirical P = ${tmp_empirical_p}"
       if [[ "${last_p}" != "${permuted_p}" ]]; then
-        if [ ${done} -eq 1 ]; then
+        if [ "${done}" -eq "1" ]; then
           readonly the_status="OK"
           readonly empirical_p=${tmp_empirical_p}
           mv "${pfile}.tmp.txt.gz" "${pfile}.txt.gz"
@@ -89,8 +82,9 @@ if [ -f ${saige_merged} ]; then
       else
         # P-value is not changing which
         # should result in termination of script
-        readonly empirical_p="NA"
+        readonly empirical_p="${tmp_empirical_p}"
         readonly the_status="OK (permuted_p not changing)"
+        mv "${pfile}.tmp.txt.gz" "${pfile}.txt.gz"
       fi
     else
       # gene not present in saige 
@@ -108,9 +102,9 @@ if [ -f ${saige_merged} ]; then
   fi 
   touch ${outfile}
   if [ "$( cat ${outfile} | wc -l)" -eq "0" ]; then 
-    echo -e "gene\tphenotype\tprs\tn_shuffle\ttrue_p\tpermuted_p\tempirical_p\tcomment" >> ${outfile}
+    echo -e "gene\tphenotype\tprs\tn_shuffle\ttrue_p\tpermuted_p\tempirical_p\titeration\tcomment" >> ${outfile}
   fi
-  echo -e "${gene}\t${phenotype}\t${prs_available}\t${n_shuffle}\t${true_p}\t${permuted_p}\t${empirical_p}\t${the_status}" >> ${outfile}
+  echo -e "${gene}\t${phenotype}\t${prs_available}\t${n_shuffle}\t${true_p}\t${permuted_p}\t${empirical_p}\t${iteration}\t${the_status}" >> ${outfile}
 else
   raise_error "Missing saige_merged: ${saige_merged}"
 fi
