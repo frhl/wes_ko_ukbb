b99a38854ceb12ad38fd8461f6707f4d17b07b99
frhl <frederik.heymann@gmail.com>
2022-10-18 11:25:17 +0100

    MOD: now also exporting MatrixTables

--- scripts/phasing/02_geno_gen.py
+++ scripts/phasing/02_geno_gen.py
@@ -96,7 +96,7 @@ def main(args):
         mt = mt.union_rows(mt2)
         final = mt.count()
         print(f"Final aggregated count: {final}")
-    
+
     if missing or min_mac or chrom in "X":
         mt = io.recalc_info(mt)
     if ancestry:
@@ -111,8 +111,11 @@ def main(args):
             mt_parents = mt.filter_cols(hl.literal(pids).contains(mt.s))
             io.export_table(mt_parents, out_prefix + "_parents", out_type)
         mt = mt.filter_cols(~hl.literal(pids).contains(mt.s))
-       
-    #io.export_table(mt, out_prefix, out_type)
+    
+    # always export matrix table 
+    if out_type != "mt":
+        io.export_table(mt, out_prefix, "mt")
+    io.export_table(mt, out_prefix, out_type)
 
 if __name__=='__main__':
 
