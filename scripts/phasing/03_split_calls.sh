69dea3d58b12b3bb6aa46732021f41eafdce6670
frhl <frederik.heymann@gmail.com>
2022-11-01 21:10:03 +0000

    MOD: Major change to file names

--- scripts/phasing/03_split_calls.sh
+++ scripts/phasing/03_split_calls.sh
@@ -11,6 +11,7 @@
 #SBATCH --cpus-per-task 3
 #SBATCH --array=1-22
 #
+#
 #$ -N prefilter_calls
 #$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
 #$ -o logs/prefilter_calls.log
@@ -20,6 +21,7 @@
 #$ -q short.qe
 #$ -t 21-22
 #$ -V
+
 
 source utils/qsub_utils.sh
 source utils/bash_utils.sh
@@ -32,11 +34,10 @@ readonly hail_script="scripts/phasing/02_prefilter_calls.py"
 readonly task_id=$( get_array_task_id )
 readonly chr=$( get_chr ${task_id} )
 
-readonly out_dir="data/unphased/calls/prefilter/test_by_maf"
-readonly out_prefix="${out_dir}/ukb_prefilter_calls_500k_chr${chr}"
+readonly out_dir="data/unphased/calls/prefilter/new_by_maf"
+readonly out_prefix="${out_dir}/ukb_prefilter_calls_200k_chr${chr}"
 readonly out_prefix_parents="${out_prefix}_parents"
 readonly out_type="vcf"
-readonly out_vcf_gz="${out_prefix}.vcf.bgz"
 
 # samples overlapping exomes and genotypes
 readonly samples_dir="data/unphased/overlap"
@@ -46,18 +47,19 @@ readonly trio_parents="${samples_dir}/ukb_calls_wes_samples_parents.txt"
 mkdir -p ${spark_dir}
 mkdir -p ${out_dir}
 
-if [ ! -f "${out_vcf_gz}" ]; then 
-  set_up_hail
+if [ ! -f "${out_prefix}.vcf.bgz" ]; then
+  set_up_hail 0.2.97
   set_up_pythonpath_legacy
   python3 "${hail_script}" \
      --chrom "${chr}" \
      --out_prefix "${out_prefix}" \
      --out_type "${out_type}" \
+     --extract_samples "${samples_list}" \
      --exclude_trio_parents "${trio_parents}" \
      --filter_incorrect_reference \
      --liftover \
-     --min_maf 0.001 \
      --export_parents \
+     --min_maf 0.001 \
      --missing 0.05 \
      --dataset "calls"
 fi
@@ -73,5 +75,8 @@ if [ -f "${out_prefix_parents}.vcf.bgz" ]; then
   module load BCFtools/1.12-GCC-10.3.0
   make_tabix "${out_prefix_parents}.vcf.bgz" "tbi"
 fi
+
+
+
 
 
---
+++ scripts/phasing/05_split_wes.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+#
+# @description filter WES quality-controlled data.
+#
+#SBATCH --account=lindgren.prj
+#SBATCH --job-name=prefilter_wes
+#SBATCH --chdir=/well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
+#SBATCH --output=logs/prefilter_wes.log
+#SBATCH --error=logs/prefilter_wes.errors.log
+#SBATCH --partition=short
+#SBATCH --cpus-per-task 2
+#SBATCH --array=21
+#
+#$ -N prefilter_wes
+#$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
+#$ -o data/help/logs/prefilter_wes.log
+#$ -e data/help/logs/prefilter_wes.errors.log
+#$ -P lindgren.prjc
+#$ -pe shmem 2
+#$ -q short.qc
+#$ -t 21
+#$ -V
+
+module purge
+source utils/qsub_utils.sh
+source utils/hail_utils.sh
+source utils/vcf_utils.sh
+
+set -x
+
+readonly spark_dir="data/help/tmp/spark"
+readonly hail_script="scripts/phasing/01_prefilter_wes.py"
+readonly hail_vcf_script="scripts/phasing/_to_mt.py"
+
+readonly array_idx=$( get_array_task_id )
+readonly chr=$( get_chr ${array_idx} )
+
+readonly in_dir="data/unphased/wes/post-qc"
+readonly in_file="${in_dir}/ukb_wes_200k_filtered_chr${chr}.mt"
+readonly in_type="mt"
+
+readonly out_dir="data/help/unphased/wes/prefilter"
+readonly out_prefix="${out_dir}/ukb_wes_prefilter_200k_chr${chr}"
+readonly out_type="vcf"
+
+readonly entry_fields_to_drop="GQ,DP,AD,PL"
+
+# samples overlapping exomes and genotypes
+readonly samples_list="data/unphased/overlap/ukb_calls_wes_samples.txt"
+
+mkdir -p ${out_dir}
+mkdir -p ${spark_dir}
+
+SECONDS=0
+set_up_hail
+set_up_pythonpath_legacy
+python3 "${hail_script}" \
+   --input_path "${in_file}" \
+   --input_type "${in_type}" \
+   --out_prefix "${out_prefix}" \
+   --out_type "${out_type}" \
+   --drop_entry_fields "${entry_fields_to_drop}" \
+   --extract_samples ${samples_list} \
+   --exclude_trio_parents \
+   --export_parents \
+   --min_mac 1 \
+   --missing 0.05
+
+#python3 "${hail_vcf_script}" \
+#   --input_path "${in_file}" \
+#   --input_type "${in_type}" \
+#   --out_prefix "${out_prefix}" \
+#   --out_type "${out_type}"
+
+module load BCFtools/1.12-GCC-10.3.0
+make_tabix "${out_prefix}.vcf.bgz" "tbi"
+
+
+
