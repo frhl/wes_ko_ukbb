8ec235ab94b82f5e2b4c1deb0a9a920a8a03404e
frhl <frederik.heymann@gmail.com>
2022-11-02 19:03:36 +0000

    FIX: Now adds index to parents and no parents VCF

--- scripts/phasing/04_prefilter_wes.sh
+++ scripts/phasing/04_prefilter_wes.sh
@@ -8,8 +8,8 @@
 #SBATCH --output=logs/prefilter_wes.log
 #SBATCH --error=logs/prefilter_wes.errors.log
 #SBATCH --partition=short
-#SBATCH --cpus-per-task 2
-#SBATCH --array=21
+#SBATCH --cpus-per-task 1
+#SBATCH --array=1-20
 #
 #$ -N prefilter_wes
 #$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
@@ -26,10 +26,8 @@ source utils/qsub_utils.sh
 source utils/hail_utils.sh
 source utils/vcf_utils.sh
 
-set -x
-
 readonly spark_dir="data/help/tmp/spark"
-readonly hail_script="scripts/phasing/01_prefilter_wes.py"
+readonly hail_script="scripts/phasing/prefilter.py"
 readonly hail_vcf_script="scripts/phasing/_to_mt.py"
 
 readonly array_idx=$( get_array_task_id )
@@ -39,8 +37,10 @@ readonly in_dir="data/unphased/wes/post-qc"
 readonly in_file="${in_dir}/ukb_wes_200k_filtered_chr${chr}.mt"
 readonly in_type="mt"
 
-readonly out_dir="data/wes/prefilter"
+readonly out_dir="data/unphased/wes/prefilter/new"
 readonly out_prefix="${out_dir}/ukb_wes_prefilter_200k_chr${chr}"
+readonly out_prefix_no_parents="${out_prefix}_no_parents"
+readonly out_prefix_parents="${out_prefix}_parents"
 readonly out_type="mt"
 
 readonly entry_fields_to_drop="GQ,DP,AD,PL"
@@ -51,19 +51,33 @@ readonly samples_list="data/unphased/overlap/ukb_calls_wes_samples.txt"
 mkdir -p ${out_dir}
 mkdir -p ${spark_dir}
 
-SECONDS=0
-set_up_hail
-set_up_pythonpath_legacy
-python3 "${hail_script}" \
-   --input_path "${in_file}" \
-   --input_type "${in_type}" \
-   --out_prefix "${out_prefix}" \
-   --out_type "${out_type}" \
-   --drop_entry_fields "${entry_fields_to_drop}" \
-   --extract_samples ${samples_list} \
-   --min_mac 1 \
-   --missing 0.05
+if [ ! -f "${out_prefix_no_parents}.vcf.bgz" ]; then
+  set_up_hail
+  set_up_pythonpath_legacy
+  python3 "${hail_script}" \
+     --input_path "${in_file}" \
+     --input_type "${in_type}" \
+     --out_prefix "${out_prefix}" \
+     --out_type "${out_type}" \
+     --drop_entry_fields "${entry_fields_to_drop}" \
+     --extract_samples ${samples_list} \
+     --min_mac 1 \
+     --missing 0.05
+else
+  >&2 echo "${out_prefix_no_parents} exists. Skipping."
+fi
 
+if [ ! -f "${out_prefix_no_parents}.vcf.bgz.tbi" ]; then
+  module purge
+  module load BCFtools/1.12-GCC-10.3.0
+  make_tabix "${out_prefix_no_parents}.vcf.bgz" "tbi"
+fi
+
+if [ -f "${out_prefix_parents}.vcf.bgz" ]; then
+  module purge
+  module load BCFtools/1.12-GCC-10.3.0
+  make_tabix "${out_prefix_parents}.vcf.bgz" "tbi"
+fi
 
 
 
