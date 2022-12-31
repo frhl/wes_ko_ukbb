89d704a7f0afd5ead3727b43c64d0a1ed7068442
frhl <frederik.heymann@gmail.com>
2022-12-31 09:13:30 +0000

    added option to evaluate shapeit4/eagle2

--- scripts/phasing/phasing/05_parents.sh
+++ scripts/phasing/phasing/05_parents.sh
@@ -11,7 +11,7 @@
 #SBATCH --constraint="skl-compat"
 #SBATCH --partition=long
 #SBATCH --cpus-per-task 4
-#SBATCH --array=1-22
+#SBATCH --array=21
 #
 #$ -N parents
 #$ -wd /well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
@@ -42,19 +42,29 @@ readonly pedigree="${pedigree_dir}/ukb11867_pedigree.fam"
 # parental genotypes that were not phased
 readonly parents_dir="data/unphased/wes_union_calls/prefilter/200k"
 readonly parents_path="${parents_dir}/ukb_wes_union_calls_chr${chr}_parents.vcf.gz"
+# fore eagle2 phasing
+#readonly phased_dir="data/phased/wes_union_calls/200k/eagle2/ligated"
+#readonly phased_path="${phased_dir}/eagle2_prs100000_pro25000_mprs150000.1of1.vcf.gz"
+#readonly out_dir="data/phased/wes_union_calls/200k/shapeit5/parents"
+#readonly out_prefix="${out_dir}/ukb_wes_union_calls_200k_eagle2_parents_chr${chr}"
+# for shapeit4 phasing
+readonly phased_dir="data/phased/wes_union_calls/200k/shapeit4/ligated"
+readonly phased_path="${phased_dir}/shapeit4_prs100000_pro25000_mprs150000.1of1.vcf.gz"
+readonly out_dir="data/phased/wes_union_calls/200k/shapeit4/parents"
+readonly out_prefix="${out_dir}/ukb_wes_union_calls_200k_shapeit4_parents_chr${chr}"
 # for shapeit5 phasing
-readonly phased_dir="data/phased/wes_union_calls/200k/shapeit5/ligated"
-readonly phased_path="${phased_dir}/ukb_wes_union_calls_200k_chr${chr}.vcf.bgz"
-readonly phased_type="vcf"
-readonly out_dir="data/phased/wes_union_calls/200k/shapeit5/parents_with_hail_count"
-readonly out_prefix="${out_dir}/ukb_wes_union_calls_200k_shapeit5_parents_chr${chr}"
+#readonly phased_dir="data/phased/wes_union_calls/200k/shapeit5/ligated"
+#readonly phased_path="${phased_dir}/ukb_wes_union_calls_200k_chr${chr}.vcf.bgz"
+#readonly out_dir="data/phased/wes_union_calls/200k/shapeit5/parents_with_hail_count"
+#readonly out_prefix="${out_dir}/ukb_wes_union_calls_200k_shapeit5_parents_chr${chr}"
 # out paths and types
+readonly phased_type="vcf"
 readonly out_vcf="${out_prefix}.vcf.gz"
 readonly out_trio="${out_prefix}.trio"
 readonly out_info="${out_prefix}.info"
 readonly out_info_gz="${out_prefix}.info.gz"
 readonly out_trio_by_site="${out_prefix}.txt"
-readonly out_trio_by_site_mac="${out_prefix}.long.mac"
+readonly out_trio_by_site_mac="${out_prefix}.mac"
 readonly out_type="vcf"
 
 mkdir -p ${out_dir}
