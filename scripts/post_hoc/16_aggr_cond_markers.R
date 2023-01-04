43e7b8125983d222472a0843b04b5129602b26f4
frhl <frederik.heymann@gmail.com>
2023-01-04 20:01:51 +0000

    fixed name and now including rare markers

--- scripts/post_hoc/16_aggr_cond_markers.R
+++ scripts/post_hoc/16_aggr_cond_markers.R
@@ -4,8 +4,8 @@ library(argparse)
 
 main <- function(args){
 
-    stopifnot(dir.exists(dirname(args$markers_dir)))
-    files  <- list.files(args$markers_dir, full.names = TRUE, pattern = ".markers$")
+    stopifnot(dir.exists(dirname(args$markers_common_dir)))
+    files  <- list.files(args$markers_common_dir, full.names = TRUE, pattern = ".markers$")
     genes <- stringr::str_extract(files, "ENSG[0-9]+")
     traits <- stringr::str_extract(files, "200k_([0-9]|[A-Z]|[a-z]|\\_)+_pLoF")
     traits <- gsub("(200k_)|(_pLoF)", "", traits)
@@ -26,19 +26,31 @@ main <- function(args){
     }))
 
     # combine the files
-    final <- cbind(stats, open_files)
-    final$files <- files
+    common <- cbind(stats, open_files)
+    common$files <- files
 
     # out
-    outfile <- paste0(args$out_prefix, ".txt.gz")
-    fwrite(final, outfile, sep = "\t", na="NA", quote=FALSE)
+    outfile <- paste0(args$out_prefix, ".common.txt.gz")
+    fwrite(common, outfile, sep = "\t", na="NA", quote=FALSE)
+    
+    stopifnot(dir.exists(dirname(args$markers_rare_dir)))   
+    files  <- list.files(args$markers_rare_dir, full.names = TRUE, pattern = ".rare.markers.full")
+    rare <- do.call(rbind, lapply(files, function(f){
+        d <- fread(f)
+        return(d)
+    }))  
         
+    # out
+    outfile <- paste0(args$out_prefix, ".rare.txt.gz")
+    fwrite(rare, outfile, sep = "\t", na="NA", quote=FALSE)
+   
 
 }
 
 # add arguments
 parser <- ArgumentParser()
-parser$add_argument("--markers_dir", default=NULL, required = TRUE, help = "Directory for markers")
+parser$add_argument("--markers_common_dir", default=NULL, required = TRUE, help = "Directory for markers")
+parser$add_argument("--markers_rare_dir", default=NULL, required = TRUE, help = "Directory for markers")
 parser$add_argument("--out_prefix", default=NULL, required = TRUE, help = "Where should the results be written?")
 args <- parser$parse_args()
 
