c2eeccd4cf3bfa7b158f140046d9d496c42514a5
frhl <frederik.heymann@gmail.com>
2023-02-06 15:19:12 +0000

    added explicit functions for creating TTE matrix with right column names

--- scripts/00_phenotypes_tte.R
+++ scripts/00_phenotypes_tte.R
@@ -6,50 +6,65 @@ library(data.table)
 
 main <- function(args){
 
-    eid_tte_matrix <- read.table("/well/lindgren-ukbb/projects/ukbb-11867/samvida/general_resources/eid_time_to_event_matrix.txt",
-                                 sep = "\t", header = T, stringsAsFactors = F)
-    colnames(eid_tte_matrix) <- gsub("^X", "", colnames(eid_tte_matrix))
+    # setup dictionary
+    dictionary <- read.table("/well/lindgren-ukbb/projects/ukbb-11867/samvida/general_resources/UKB_codelists/chronological-map-phenotypes/annot_dictionary_with_unix_codes.txt",
+                         sep = "\t", header = T, stringsAsFactors = F, 
+                         quote = "", comment.char = "$")
 
-    # Disease dictionary
-    dictionary <- read.table("/well/lindgren-ukbb/projects/ukbb-11867/samvida/general_resources/UKB_codelists/chronological-map-phenotypes/annot_dictionary.txt",
-                             sep = "\t", header = T, stringsAsFactors = F, 
-                             quote = "", comment.char = "$")
-    DISEASES <- dictionary$phenotype[match(colnames(eid_tte_matrix)[c(-1:-3)],
-                                           dictionary$unique_code)]
-    colnames(eid_tte_matrix)[c(-1:-3)] <- DISEASES
-    d <- eid_tte_matrix
+    # In this matrix, columns 6:349 are the Spiros and Duncan disease codes
+    # Column names are UNIX names from the dictionary
+    tte_matrix <- read.table("/well/lindgren-ukbb/projects/ukbb-11867/samvida/general_resources/all_phenos_time_to_event.txt",
+                                     sep = "\t", header = T, stringsAsFactors = F,
+                                     quote = "")
 
-    d <- cbind(eid_tte_matrix$eid, eid_tte_matrix[,colnames(eid_tte_matrix) %in% DISEASES])
-    colnames(d)[1] <- "eid"
+    # In this matrix, columns 2:308 are the Spiros disease codes
+    # Columns need to be renamed to UNIX names from the dictionary
+    pheno_matrix <- read.table("/well/lindgren-ukbb/projects/ukbb-11867/samvida/general_resources/eid_phenotype_matrix.txt", 
+                                   sep = "\t", quote = "",
+                                   header = T, stringsAsFactors = F)
+    rownames(pheno_matrix) <- pheno_matrix$eid
+    pheno_matrix <- pheno_matrix[, -1]
+    colnames(pheno_matrix) <- dictionary$unix_code
 
-    # rename samvida's phenotypes
-    encoding <- fread("data/phenotypes/phenotype_icd_chapter.txt")
-    stopifnot(any(colnames(d) %in% encoding$phenotype))
+    # subset tte_matrix
+    samples <- fread(args$samples, header = FALSE)
+    tte_matrix <- tte_matrix[tte_matrix$eid %in% samples,]
+    
+    # rename columns based on current definitions
+    header <- fread(args$path_header, header = FALSE)$V1
 
-    # match with spiro phenotyoes
-    thematch <- encoding$unix_code[match(colnames(d), encoding$phenotype)]
-    not_na <- which(!is.na(thematch))
-    colnames(d)[not_na] <- thematch[not_na]
+    # remap to universal spiro naming in the project
+    spiro_to <- header[grepl(pattern='spiro', header)]
+    spiro_from <- gsub("spiro_","",spiro_to)
+    spiro_map <- spiro_to
+    names(spiro_map) <- spiro_from
 
-    # clean up
-    d <- d[,!grepl(pattern="NA",colnames(d))]
-    d <- d[,-2]
-    outfile <- "data/phenotypes/jan23_tte_spiro_phenotypes.txt.gz"
-    fwrite(d, outfile, sep = '\t')
+    # remap to unversal palmer naming in the project
+    palmer_to <- header[!grepl(pattern='spiro', header)]
+    palmer_to <- palmer_to[grepl("combined", palmer_to)]
+    palmer_to <- palmer_to[!grepl("primary_care", palmer_to)]
+    palmer_from <- gsub("_combined(_primary_care)?","",palmer_to)
+    palmer_map <- palmer_to
+    names(palmer_map) <- palmer_from 
 
-    # setup boolean matrix (i.e. discard tte)
-    d_bool <- data.table(!is.na(d[,-1]))
-    d_bool$eid <- d$eid
-    d_bool <- d_bool[,c("eid",colnames(d_bool)[-ncol(d_bool)]), with =FALSE]
-    outfile <- "data/phenotypes/jan23_tte_spiro_phenotypes_logical.txt.gz"
-    fwrite(d_bool, outfile, sep = '\t')
+    # change spiro columns
+    cnames_to_change_spiro <- colnames(tte_matrix)[colnames(tte_matrix) %in% spiro_from]
+    colnames(tte_matrix)[match(cnames_to_change_spiro, colnames(tte_matrix))] <- spiro_map[cnames_to_change_spiro]
 
+    # change palmer columns
+    cnames_to_change_palmer <- colnames(tte_matrix)[colnames(tte_matrix) %in% palmer_from]
+    colnames(tte_matrix)[match(cnames_to_change_palmer, colnames(tte_matrix))] <- palmer_map[cnames_to_change_palmer]
+
+    write(paste("writing to", args$out_path), stdout())
+    fwrite(tte_matrix, args$out_path)
 
 }
 
 # add arguments
 parser <- ArgumentParser()
-parser$add_argument("--tbd", default=NULL, help = "")
+parser$add_argument("--out_path", default=NULL, help = "")
+parser$add_argument("--path_header", default=NULL, help = "")
+parser$add_argument("--samples", default=NULL, help = "")
 args <- parser$parse_args()
 
 main(args)
--- scripts/00_samples.py
+++ scripts/00_samples.py
@@ -13,15 +13,18 @@ def main(args):
     export_imputed_samples = args.export_imputed_samples
     export_genotyped_samples = args.export_genotyped_samples 
     get_unrelated = args.get_unrelated
+    remove_withdrawn = args.remove_withdrawn
     extract1 = args.extract1
     extract2 = args.extract2
-
 
     if export_imputed_samples:
         mt = genotypes.get_ukb_imputed_v3_bgen([21])
 
     if export_genotyped_samples:
         mt = genotypes.get_ukb_genotypes_bed([21])
+
+    if remove_withdrawn:
+        mt = samples.remove_withdrawn(mt)
 
     if extract1:
         ht_samples = hl.import_table(
@@ -53,6 +56,7 @@ if __name__=='__main__':
     parser.add_argument('--export_imputed_samples', action='store_true', default=None, help='')
     parser.add_argument('--export_genotyped_samples', action='store_true', default=None, help='')
     parser.add_argument('--get_unrelated', action='store_true', default=None, help='')
+    parser.add_argument('--remove_withdrawn', action='store_true', default=None, help='')
     parser.add_argument('--extract1', default=None, help='')
     parser.add_argument('--extract2', default=None, help='')
 
