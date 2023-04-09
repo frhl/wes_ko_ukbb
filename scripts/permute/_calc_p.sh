5c2ce92e1cf141bb2ddebf6b75472d636c85bf34
frhl <frederik.heymann@gmail.com>
2023-04-09 18:37:06 +0100

    functional and tested pipeline

--- scripts/permute/_calc_p.sh
+++ scripts/permute/_calc_p.sh
@@ -49,6 +49,7 @@ if [ "${total_phenos}" -ne "${tested_phenos}" ]; then
   if [ -f "${saige_merged}" ]; then
     set_up_rpy
     if [ "$(zcat ${saige_merged} | wc -l)" -gt "1" ]; then
+      # strange error with argparse below so needed to use commandArgs instead.
       readonly true_p=$( Rscript ${r_get_true_p} "${saige_merged}" )
       if [[ "${true_p}" != "NA" ]]; then
         readonly last_p=$(get_last_permuted_p)
