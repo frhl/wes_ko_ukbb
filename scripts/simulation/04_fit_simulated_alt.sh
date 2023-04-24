1455a96e64d894139c56206984cf7f46992575c0
frhl <frederik.heymann@gmail.com>
2023-04-24 14:54:40 +0100

    fixed simulate alt

--- scripts/simulation/04_fit_simulated_alt.sh
+++ scripts/simulation/04_fit_simulated_alt.sh
@@ -14,28 +14,25 @@ set -o errexit
 set -o nounset
 
 source utils/bash_utils.sh
+source utils/qsub_utils.sh
+
+# array idx indicate phenotype idx
+readonly array_idx=$( get_array_task_id )
+readonly chr=22
 
 readonly vcf_dir="data/simulation/knockouts"
 readonly pheno_dir="data/phenotypes"
 readonly spark_dir="data/tmp/spark"
 
 readonly spa_script="scripts/_spa_test.sh"
-readonly merge_script="scripts/_spa_merge.sh"
-readonly vcf_prefix="ukb_wes_union_calls_100k_encoded_chr22"
+#readonly merge_script="scripts/_spa_merge.sh"
+readonly vcf_prefix="ukb_wes_union_calls_100k_encoded_chr${chr}"
 
 readonly grm_dir="data/saige/grm/input/dnanexus"
 readonly grm_mtx="${grm_dir}/ukb_eur_200k_grm_fitted_relatednessCutoff_0.05_2000_randomMarkersUsed.sparseGRM.mtx"
 readonly grm_sam="${grm_mtx}.sampleIDs.txt"
 
 readonly conditioning_markers=""
-readonly project="lindgren.prj"
-readonly min_mac=10
-readonly chr=22
-readonly tasks=${chr}
-readonly queue="short"
-readonly nslots=1
-
-readonly array_idx=$( get_array_task_id )
 
 # wrapper for running main script
 run_with_params() {
@@ -51,10 +48,11 @@ submit_spa()
   local pi=${4}
   local seed=${5}
 
+
   # change "case" to "y" for quant
   local prefix="ukb_wes_union_calls_h2_${h2}_b_${b}_pi_${pi}_K${K}_seed${seed}_chr${chr}"
   local saige_prefix="${prefix}_case_${array_idx}"
-  
+
   #local phenotype="y_${array_idx}"
   local phenotype="case_${array_idx}"
   submit_spa_with_csqs "${phenotype}" "${saige_prefix}" "binary"
@@ -77,11 +75,11 @@ submit_spa_with_csqs()
 
 
 submit_spa_job() {
-  set -x
   >&2 echo "Submitting SPA Job"
   mkdir -p ${step2_dir}
-  local jname="_spa_job"
-  local lname="logs/_spa_job"
+  local jname="_fit_simulated_alt"
+  local lname="logs/_fit_simulated_alt"
+  set -x
   local sim_job=$( sbatch \
     --account="${project}" \
     --job-name="${jname}" \
@@ -90,21 +88,29 @@ submit_spa_job() {
     --chdir="$(pwd)" \
     --partition="${queue}" \
     --cpus-per-task="${nslots}" \
-    --array="${tasks}" \
     --parsable \
-     ${bash_script} \
-     ${rscript} \
-     ${plink_file} \
-     ${pheno_file} \
-     ${phenotype} \
-     ${covariates} \
-     ${trait_type} \
-     ${inv_normalize} \
-     ${out_prefix} \
-     ${grm_mtx} \
-     ${grm_sam} \
-     ${chr} )
+    "${spa_script}" \
+    "${phenotype}" \
+    "${in_vcf}" \
+    "${in_vcf}.csi" \
+    "${in_gmat}" \
+    "${in_var}" \
+    "${grm_mtx}" \
+    "${grm_sam}" \
+    "${min_mac}" \
+    "${out_prefix}.txt" \
+    "${conditioning_markers}")
 }
 
+readonly min_mac=10
+readonly project="lindgren.prj"
+readonly queue="short"
+readonly nslots=1
+
+#run_with_params 0.2 0.00 0.5 0.20 101
+run_with_params 0.2 0.01 0.5 0.20 101
+#run_with_params 0.2 0.05 0.5 0.20 101
+#run_with_params 0.2 0.05 10.0 0.20 101
+#run_with_params 0.2 0.05 0.0 0.20 101
 
 
