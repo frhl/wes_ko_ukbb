814b3202eb1b2c07e2e9e50b86a86502a7161122
frhl <frederik.heymann@gmail.com>
2024-01-22 21:55:05 +0000

    checkpoint before BMRC shutdown

--- scripts/post_hoc/simulation/01_simulate_ch_events.sh
+++ scripts/post_hoc/simulation/01_simulate_ch_events.sh
@@ -5,9 +5,9 @@
 #SBATCH --chdir=/well/lindgren-ukbb/projects/ukbb-11867/flassen/projects/KO/wes_ko_ukbb
 #SBATCH --output=logs/simulate_ch_events.log
 #SBATCH --error=logs/simulate_ch_events.errors.log
-#SBATCH --partition=short
-#SBATCH --cpus-per-task 2
-
+#SBATCH --partition=epyc
+#SBATCH --cpus-per-task 1
+#SBATCH --time=12:00:00
 
 set -o errexit
 set -o nounset
@@ -18,22 +18,11 @@ source utils/vcf_utils.sh
 
 readonly rscript="scripts/post_hoc/simulation/01_simulate_ch_events.R"
 
-readonly vep_dir="data/vep/vep95/worst_csqs"
-readonly vep_file="${vep_dir}/UKB.chrCHR.exome_array.variants_only.vep95.csqs.worst_csq_by_gene_canonical.original.txt.gz"
-
-readonly in_dir="data/mt/prefilter/pp90"
-readonly in_file="${in_dir}/ukb_wes_union_calls_200k_chrCHR.loftee.worst_csq_by_gene_canonical.pp90.maf0_005.from_mt.alt_alleles.txt.gz"
-
-readonly out_dir="data/mt/singleton_category_count"
-readonly out_prefix="${out_dir}/ukb_wes_union_calls_200k.loftee.worst_csq_by_gene_canonical.pp90.maf0_005.from_mt.alt_alleles.singleton_category"
 
 mkdir -p ${out_dir}
 
 set_up_rpy
-Rscript ${rscript} \
-  --vep_file ${vep_file} \
-  --in_file ${in_file} \
-  --out_prefix ${out_prefix}
+Rscript ${rscript}
 
 
 
